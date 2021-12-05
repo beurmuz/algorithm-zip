1ebee914e168e7a69fa504198d09176e98e9b202
beurmuz <fallinta2@gmail.com>
2021-12-05 14:57:13 +0900

    폴더명 변경

--- src/backjoon/level1_9/bj2588.js
+++
@@ -1,9 +0,0 @@
-// 2588번, 곱셈
-
-'use strict'
-const num1 = 472;
-const num2 = 385;
-console.log(num1*5);
-console.log(num1*80);
-console.log(num1*300);
-console.log(num1*num2);
--- src/backjoon/level1_9/bj2753.js
+++
@@ -1,9 +0,0 @@
-// 2753번, 윤년
-
-'use strict'
-const whatYear = 1999;
-if(whatYear%4===0 && (whatYear%100!=0 || whatYear%400===0)) {
-    console.log(1)
-} else {
-    console.log(0);
-}
