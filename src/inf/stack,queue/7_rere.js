42cd76a1b946f6e71db26d43ce0c24e1e051669a
beurmuz <fallinta2@gmail.com>
2022-05-07 23:22:18 +0900

    #6 22.05.07 > 7번다시 풀기

---
+++ src/inf/stack,queue/7_rere.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function solution(needSubject, designSubject) {
+    let answer = 'YES';
+    needSubject = needSubject.split('');
+    for(let i of designSubject) {
+        if(needSubject.includes(i)) {
+            if(i !== needSubject.shift()) {
+                return "NO";
+            }
+        }
+    }
+    if(needSubject.length > 0) {
+        return "NO";
+    }
+
+    
+
+
+    return answer;
+}
+
+
+let needSubject = 'CBA';
+let designSubject = 'CBDAGE';
+
+console.log(solution(needSubject, designSubject));
+
+/*
+
+*/
