cb1f70c7ebeb19d091fc65e1f64f46d147e85297
beurmuz <fallinta2@gmail.com>
2022-05-10 12:02:46 +0900

    #7 22.05.10 > 삽입정렬

--- src/inf/sort,greedy/4.js
+++ src/inf/sort,greedy/4.js
@@ -2,15 +2,27 @@
 
 function solution(arr){
     let answer=arr;
-    for(let j = 0; j < answer.length; j++) {
-        for(let i = 0; i < answer.length; i++) {
-            if(answer[i] > answer[i+1]) {
-                [answer[i], answer[i+1]] = [answer[i+1], answer[i]];
+    for(let i = 0; i < arr.length; i++) {
+        let tmp = arr[i], j;
+        for(j = i-1; j >= 0; j--) {
+            if(arr[j] > tmp) {
+                arr[j+1] = arr[j];
+            } else {
+                break;
             }
         }
+        arr[j+1] = tmp;
+        console.log(answer);
     }
     return answer;
 }
 
 let arr=[11, 7, 5, 6, 10, 9];
 console.log(solution(arr));
+
+/*
+    - i는 1부터 arr.length까지 반복
+    - tmp 배열에 arr[i]를 임시로 저장해놓고, j는 i-1부터 0까지 반복함
+        - j는 i의 앞을 도는 것
+*/
+
