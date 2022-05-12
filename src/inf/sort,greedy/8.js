34576bc324a24cf33569914ccc2a8a45dcd59bea
beurmuz <fallinta2@gmail.com>
2022-05-13 08:42:51 +0900

    #7 22.05.13 > 8버 회의실문제

--- src/inf/sort,greedy/8.js
+++ src/inf/sort,greedy/8.js
@@ -1,24 +1,28 @@
 'use strict';
 
 function solution(meeting){
-    let answer=0;
-    meeting.sort((a,b) => {
-        if(a[1]===b[1]) {
-            return a[0]-b[0];
-        } else {
-            return a[1]-b[1];
-        }
-    })
-    let endTime = 0;
+    let answer=[];
+    meeting.sort((a, b)=>{
+        if(a[1]===b[1]) return a[0]-b[0];
+        else return a[1]-b[1];
+    });
+    console.log(meeting);
+    let element = 0;
     for(let x of meeting) {
-        if(x[0] >= endTime) {
-            console.log(`${x[0]}~${x[1]}시 가능`);
-            answer++;
-            endTime = x[1]; // 회의가 끝나는 시간으로 업데이트 
+        if(x[0] >= element) {
+            answer.push(x);
+            element = x[1];
         }
     }
-    return answer;
+    return answer.length;
 }
 
-let arr=[[1, 4], [2, 3], [3, 5], [4, 6], [5, 7]];
-console.log(solution(arr));
+let meeting=[[1, 4], [2, 3], [3, 5], [4, 6], [5, 7]];
+console.log(solution(meeting));
+
+/*
+    1. 우선 meeting이 끝나는 시간에 따라 오름차순 정렬하기
+        - 끝나는 시간이 같으면 시작 시간에 따라 정렬하기
+    2. x[1]을 저장할 element 변수 선언
+    3. meeting 배열을 순회하면서 x[0]이 앞의 x[1]보다 큰 경우 
+*/
