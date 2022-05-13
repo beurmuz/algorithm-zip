139f756ea8da5dc89e9d286662ffed12ea91e6fd
beurmuz <fallinta2@gmail.com>
2022-05-13 14:19:12 +0900

    #7 22.05.13 > 9번문제

--- src/inf/sort,greedy/9.js
+++ src/inf/sort,greedy/9.js
@@ -2,30 +2,34 @@
 
 function solution(times){
     let answer=0;
-    let T_line=[];
-    for(let x of times){
-        T_line.push([x[0], 'start']);
-        T_line.push([x[1], 'end']);
+    let timeTable = [];
+    let count = 0;
+    for(let x of times) {
+        timeTable.push([x[0], 's']); // 시작시간 
+        timeTable.push([x[1], 'e']); // 종료시간
     }
-    console.log(...T_line);
-    T_line.sort((a,b) => {
-        if(a[0]===b[0]) {
-            return a[1].charCodeAt()-b[1].charCodeAt();
+    
+    timeTable.sort((a, b) => {
+        if(a[0] === b[0]) { // 시간이 같으면 e -> s 순서로 정렬
+            return a[1].charCodeAt()-b[1].charCodeAt(); // s, e를 아스키코드로 변환해야함
         } else {
             return a[0]-b[0];
         }
     });
-    let count = 0;
-    for(let x of T_line) {
-        if(x[1]==='start') {
-            count++;
-        } else {
-            count--;
-        }
-        answer = Math.max(answer,count);
+    console.log(...timeTable);
+
+    for(let x of timeTable) {
+        if(x[1] === 's') count++;
+        else count--;
+        answer = Math.max(answer, count);
     }
+
     return answer;
 }
 
 let arr=[[14, 18], [12, 15], [15, 20], [20, 30], [5, 14]];
 console.log(solution(arr));
+
+/*
+
+*/
