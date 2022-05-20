ed1b044d9a9e89b9dac8c03f885bef9fb122b326
beurmuz <fallinta2@gmail.com>
2022-05-21 07:59:16 +0900

    #8 22.05.21 > 6번 문제 풀기

---
+++ src/inf/recursive,dfs/6.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function solution(limitWeigth, arr) {
+    let answer = Number.MIN_SAFE_INTEGER;
+    let n = arr.length;
+    let flag = 0;
+    function dfs(L, sum) {
+        if(sum > limitWeigth) return;
+        if(L === n) {
+            answer = Math.max(answer, sum); // 최댓값이 생기면 그걸로 항상 갱신해주기
+        } else {
+            // 바둑이를 태운다
+            dfs(L+1, sum+arr[L]);
+
+            // 바둑이를 태우지 않는다.
+            dfs(L+1, sum);
+        }
+    }
+    dfs(0, 0);
+    return answer;
+}
+
+let arr = [81, 58, 42, 33, 61];
+console.log(solution(259, arr));
+
+/*
+    부분집합의 합문제 
+    - 보통 두 갈래로 나뉘는 문제는 dfs로 부분집합 구현해서 풀기
+*/
