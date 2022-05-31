3ed1324b6d6d402d165078786ecebbb7acaf8daa
beurmuz <fallinta2@gmail.com>
2022-05-31 11:35:59 +0900

    #12 22.05.31 > 최대공약수와 최소공배수

---
+++ src/programmers/Lv_1/GcdLcm.js
@@ -0,0 +1,26 @@
+function solution(n, m) {
+    var answer = [];
+
+    // 최대공약수
+    function getGcd() {
+        let gcd = 1;
+        for(let i = 2; i <= Math.min(n, m); i++) {
+            if(n%i === 0 && m%i === 0) gcd = i;
+        }
+        return gcd;
+    } 
+
+    // 최소 공배수
+    function getLcm() {
+        let lcm = 1;
+        while(1) {
+            if(lcm%n === 0 && lcm%m === 0) break;
+            lcm++;
+        }
+        return lcm;
+    }
+
+    answer.push(getGcd(n,m));
+    answer.push(getLcm(n,m));
+    return answer;
+}
