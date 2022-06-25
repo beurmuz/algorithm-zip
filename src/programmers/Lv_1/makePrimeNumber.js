bd1db26e6e56a678f37261e080e6d947708e01de
beurmuz <fallinta2@gmail.com>
2022-06-26 07:43:17 +0900

    #12 22.06.26 > 소수 만들기 다시 풀어보기

--- src/programmers/Lv_1/makePrimeNumber.js
+++ src/programmers/Lv_1/makePrimeNumber.js
@@ -25,3 +25,26 @@ function isPrime(value) {
     }
     return value > 1;
 }
+
+
+// 22.06.26 다시 풀기
+function solution(nums) {
+    let answer = 0;
+    
+    for(let i = 0; i <= nums.length; i++) {
+        for(let j = i+1; j <= nums.length; j++) {
+            for(let k = j+1; k <= nums.length; k++) {
+                let sum = nums[i] + nums[j] + nums[k];
+                if(isPrime(sum)) answer += 1;
+            }
+        }
+    }
+    return answer;
+}
+
+function isPrime(v) {
+    for(let i = 2; i <= Math.sqrt(v); i++) {
+        if(v % i === 0) return false;
+    }
+    return v >= 2;
+}
