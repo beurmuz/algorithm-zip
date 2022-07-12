4f7e8ac83376dc381d0c3fd66f75cf03693f1d6d
beurmuz <fallinta2@gmail.com>
2022-07-12 11:39:41 +0900

    #17 폴더추가

--- src/bj/bronze/2/1009.js
+++ src/bj/bronze/2/1009.js
@@ -4,7 +4,7 @@
     생각해보면 매 값을 제곱해줄 때마다 %10을 해주면 그 값은 늘 같게 나온다는 점을 활용하면 시간 초과 문제를 해결할 수 있다.
 */
 // const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const input = require('fs').readFileSync('./1009.txt').toString().trim().split('\n');
+const input = require('fs').readFileSync('./data/1009.txt').toString().trim().split('\n');
 const T = input.shift('')*1;
 
 const result = input.map((v) => {
