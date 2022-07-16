f96e9caca7fb1a7305443056ea26b2983f3ec9bf
beurmuz <fallinta2@gmail.com>
2022-07-16 15:00:37 +0900

    #21 22.07.16 > 구현 4번문제 복습

--- src/cote/implementation/imple_02.js
+++ src/cote/implementation/imple_02.js
@@ -1,30 +1,29 @@
 'use strict';
 
 function solution(N, M, x, y, d, map) {
-    // 방문한 위치를 저장할 map
-    const visitedMap = Array.from(Array(N), () => Array(M).fill(0)); 
-
-    visitedMap[x][y] = 1; // 방문 처리
+    const visitedMap = Array.from(Array(N), () => Array(M).fill(0)); // 방문한 위치를 저장할 map
+    visitedMap[x][y] = 1; // 현재 위치 방문 처리
     
-    // 북, 동, 남, 서
+    // 북(-1, 0), 동(0, -1), 남(1, 0), 서(0, 1)
     const dx = [-1, 0, 1, 0];
     const dy = [0, -1, 0, 1];
 
-    let direction = 0;
+    // 캐릭터가 바라보고 있는 방향 -> 북(0), 동(1, 오른쪽), 남(2), 서(3, 왼쪽)
+    let direction = d;  
 
-    // 왼쪽으로 회전
+    // 왼쪽으로 회전 -> 현재 위치에서 -1씩해야 반시계 방향으로 회전함
     function turnLeft() {
-        direction -= 1;
-        if(direction == -1) direction = 3;
+        direction = direction - 1;
+        if(direction === -1) direction = 3; // 0에서 -1로 갔다는 건 서쪽을 바라보고 있다는 뜻
     }
 
     // 시뮬레이션
-    let count = 1;
+    let count = 1; // 캐릭터가 방문한 칸의 수 세기
     let turn_time = 0;
 
     while(1) {
-        turnLeft();
-        let nx = x + dx[direction];
+        turnLeft(); // 왼쪽 방향으로 회전
+        let nx = x + dx[direction]; // 
         let ny = y + dy[direction];
 
         // 회전 후 정면에 가보지 않은 칸이 존재하는 경우 이동함
@@ -62,3 +61,11 @@ let map = [[1, 1, 1, 1],
            [1, 1, 0, 1],
            [1, 1, 1, 1]];
 console.log(solution(4, 4, 1, 1, 0, map));
+
+/*
+    - 캐릭터는 동서남북 중 한 곳을 바라봄
+    1. 현재 위치, 방향을 기준으로 왼쪽 방향 (반시계 방향으로)부터 이동할 곳 정하기
+    2. 캐릭터의 바로 왼쪽에 아직 방문하지 않은 곳이 있다면 => 왼쪽 방향으로 회전(바라본) 후, 앞으로 한 칸 전진
+    3. 4방향 모두 방문했거나 / 바다이면 => 바라본 방향을 유지한 채 뒤로 1칸 이동 후, 다시 1번부터 실행
+        - 만약 뒷칸이 바다이면 => 움직임을 멈춤 
+*/
