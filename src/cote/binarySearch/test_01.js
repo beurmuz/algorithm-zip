7a285c5684b3413fece6005f6918217f36eced48
beurmuz <fallinta2@gmail.com>
2022-07-29 07:54:40 +0900

    #21 22.07.29 > 이진탐색 > 정렬문제 해설보기

--- src/cote/binarySearch/test_01.js
+++ src/cote/binarySearch/test_01.js
@@ -1,15 +1,30 @@
 'use strict';
 
-function solution(n, x, arr) {
-    let count = 0;
-    function binarySearch(arr, target, start, end) {
-        let mid = Math.ceil(start+end/2);
-        if(arr[mid] === target) count++;
-        if(arr[mid] > target) end = mid - 1;
-        else if(arr[mid] < target) start = mid + 1;
-    }
-    return count;
+const first = (array, target, start, end) => {
+  if (start > end) return -1;
+
+  const mid = Math.floor((start + end) / 2);
+  if (mid === 0 || array[mid - 1] < target && array[mid] === target) return mid;
+  else if (array[mid] < target) return first(array, target, mid + 1, end);
+  else return first(array, target, start, mid - 1);
 }
 
-const numberList = [1, 1, 2, 2, 2, 2, 3];
-console.log(solution(7, 2, numberList));
+const last = (array, target, start, end) => {
+  if (start > end) return -1;
+  const mid = Math.floor((start + end) / 2);
+  if (mid === array.length - 1 || array[mid + 1] > target && array[mid] === target) return mid;
+  else if (array[mid] <= target) return last(array, target, mid + 1, end);
+  else return last(array, target, start, mid - 1);
+}
+
+const solution = (N, x, datas) => {
+  const f = first(datas, x, 0, N);
+  if (f === -1) {
+    console.log(-1);
+    return;
+  }
+  const l = last(datas, x, 0, N);
+  console.log(l - f + 1);
+}
+
+solution(7, 2, [1, 1, 2, 2, 2, 2, 3]);
