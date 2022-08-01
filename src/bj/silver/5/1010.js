8649dcd27f11b5ce5e6be8fb82946e8a64caedb7
beurmuz <fallinta2@gmail.com>
2022-08-02 07:35:48 +0900

    폴더 추가

--- src/bj/silver/5/1010.js
+++ src/bj/silver/5/1010.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-const file = process.platform === 'linux' ? '/dev/stdin' : './data/파일명';
+const file = process.platform === 'linux' ? '/dev/stdin' : './data/1010.txt';
 const input = fs.readFileSync(file).toString().trim().split('\n');
 
 let inputArr = [];
