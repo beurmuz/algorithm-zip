57156d6fb0eb750094a2b3ce4012b20c8010372b
beurmuz <fallinta2@gmail.com>
2022-08-02 07:32:23 +0900

    #18 22.08.02 > 5 > 다리놓기

---
+++ src/bj/silver/1010.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './data/파일명';
+const input = fs.readFileSync(file).toString().trim().split('\n');
+
+let inputArr = [];
+for (let i = 1; i < input.length; i++) {
+  inputArr.push(input[i].split(" "));
+}
+
+soultion(inputArr);
+function soultion(inputArr) {
+  let answer = [];
+  inputArr.map((el) => {
+    const n = +el[0];
+    const m = +el[1];
+    const memo = Array.from(Array(30), () => Array(30).fill(0));
+
+    for (let i = 1; i <= n; i++) {
+      for (let j = i; j <= m; j++) {
+        if (j === i) {
+          memo[i][j] = 1;
+        } else if (i === 1) {
+          memo[i][j] = j;
+        } else {
+          let temp = 0;
+          for (let k = 1; k < j; k++) {
+            temp += memo[i - 1][k];
+          }
+          memo[i][j] = temp;
+        }
+      }
+    }
+    answer.push(memo[n][m]);
+  });
+  console.log(answer.join("\n"));
+}
