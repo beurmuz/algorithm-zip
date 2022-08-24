4c335b61af633aff57b26a4d4bf064cc595165bd
beurmuz <fallinta2@gmail.com>
2022-08-24 20:56:07 +0900

    #13 22.08.24 > H-Index

---
+++ src/programmers/Lv_2/hindex.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function solution(citations) {
+    let answer = 0;
+    citations.sort((a, b) => b-a);
+    let h = 0;
+    while(h+1 <= citations[h]) {
+        h++;
+    }
+    answer = h
+    return answer;
+}
