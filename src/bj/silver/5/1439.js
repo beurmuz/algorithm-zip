e37500f55c3e0518fc298d578bdfe2133eaa3b10
beurmuz <fallinta2@gmail.com>
2022-08-30 20:52:00 +0900

    #18 22.08.30 > 5 > 문자열 뒤집기

---
+++ src/bj/silver/5/1439.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const input = require('fs').readFileSync('test.txt').toString().trim();
+function solution(input) {
+    if(input.includes(0) && input.includes(1)) {
+        let toOne = input.split(0).filter(el => el!== '').length;
+        let toZero = input.split(1).filter(el => el!== '').length;
+        return toOne < toZero ? toOne : toZero;
+    } else return 0
+};
+console.log(solution(input));
