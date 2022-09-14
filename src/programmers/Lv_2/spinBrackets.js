fba0e0c462f20e9430778e8de9cd9d70243d201f
beurmuz <fallinta2@gmail.com>
2022-09-15 07:31:06 +0900

    #13 22.09.15 > 괄호 회전하기

---
+++ src/programmers/Lv_2/spinBrackets.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// 다른 풀이
+function solution(s) {
+    let answer = 0;
+    let sArr = s.split('')
+    if(isRight(sArr.join(''))) answer += 1
+    for(let i=0; i<sArr.length - 1; i++){
+        sArr.push(sArr.shift())
+        if(isRight(sArr.join(''))) answer += 1
+    }
+    return answer;
+}
+function isRight(str){
+    const bracketOpen = ['[', '{', '('], bracketClose = [']', '}', ')']
+    let status = [{open: false, openOrder: []}, {open: false, openOrder: []}, {open: false, openOrder: []}]
+
+    for(let sIndex=0; sIndex<str.length; sIndex++){
+        for(let bIndex = 0; bIndex<3; bIndex++){
+            if(str[sIndex] === bracketOpen[bIndex]){
+                status[bIndex].open = true
+                status[bIndex].openOrder.push(sIndex)
+            }
+            if(str[sIndex] === bracketClose[bIndex]){
+                if(status[bIndex].openOrder.length > 0){
+                    if(status.filter(check=>check.open && check.openOrder[check.openOrder.length - 1] > status[bIndex].openOrder[status[bIndex].openOrder.length - 1]).length > 0)
+                        return false //먼저 닫혀야 하는 괄호보다 먼저 닫힘
+                    status[bIndex].openOrder.pop()
+                    status[bIndex].open = false
+                }else{
+                    return false //열리기 전에 닫힘
+                }
+            }
+        }
+    }
+    for(let i=0; i<3; i++) if(status[i].open) return false //닫히지 않은 괄호가 있음
+    return true
+}
+
+
+// 다른 풀이
+function solution(s) {
+  let answer = 0;
+  for (let i = 0; i < s.length; i++) {
+    const arr = [];
+    const temp = i === 0 ? s : s.slice(i) + s.slice(0, i);
+    for (let j = 0; j < temp.length; j++) {
+      if (arr[arr.length - 1] === '(' && temp[j] === ')') arr.pop();
+      else if (arr[arr.length - 1] === '[' && temp[j] === ']') arr.pop();
+      else if (arr[arr.length - 1] === '{' && temp[j] === '}') arr.pop();
+      else arr.push(temp[j]);
+    }
+    if (arr.length === 0) answer++;
+  }
+  return answer;
+}
