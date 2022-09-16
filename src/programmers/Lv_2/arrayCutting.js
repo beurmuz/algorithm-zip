b8172c7924c2bafc04c7966080849b02c82e7f19
beurmuz <fallinta2@gmail.com>
2022-09-16 13:04:41 +0900

    #13 22.09.16 > n^2 배열 자르기 모르겠다

--- src/programmers/Lv_2/arrayCutting.js
+++ src/programmers/Lv_2/arrayCutting.js
@@ -1,14 +1,13 @@
 'use strict';
+// 어 이해 안되는데요
 
 function solution(n, left, right) {
-    let arr = []
-  
-    for (let i = left; i <= right; i++) {
-      let j = Math.floor(i / n) - i % n
-      j = j < 0 ? 0 : j
-      arr.push(i % n + 1 + j)
+    let arr = [];
+    for(let i = left; i <= right; i++) {
+        let j = Math.floor(i/n) - i%n;
+        j = j < 0 ? 0 : j;
+        arr.push(i%n+1+j);
     }
-  
     return arr;
 }
 
