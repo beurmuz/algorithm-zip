47d278e5f94bc31fc8ee972acb9d3e665333df18
beurmuz <fallinta2@gmail.com>
2022-09-23 15:20:28 +0900

    #13 22.09.23 > 방문 길이

--- src/programmers/Lv_2/visitLength.js
+++ src/programmers/Lv_2/visitLength.js
@@ -37,3 +37,54 @@ const move = ([x, y], dir) => {
         if (next[0] < 0 || next[0] > 10 || next[1] < 0 || next[1] > 10) return [x, y];
         return next;
 }
+
+
+// 다시 풀어보기
+/*
+    코드를 더 쉽게 바꿔보았지만 테케는 1~7만 통과하고, 정확성은 35.0이다.
+*/
+function solution(dirs) {
+    // 범위를 -5~5가 아닌 0~10으로 잡고 (5,5)를 기준으로 잡기
+    let visited = {};
+    let now = [5, 5];
+
+    dirs.split('').forEach((dir) => {
+        let next = [];
+        
+        if(dir === 'U') next = [now[0], now[1]+1];
+        if(dir === 'D') next = [now[0], now[1]-1];
+        if(dir === 'R') next = [now[0]+1, now[1]];
+        if(dir === 'L') next = [now[0]-1, now[1]];
+        
+        if(next[0] >= 0 && next[0] <= 10 && next[1] >= 0 && next[1] <= 10) {
+            visited[[now, next]] = true;
+            // visited[[next, now]] = true;
+            now = next;
+        }
+    });
+    
+    // console.log(visited);
+    return Object.keys(visited).length;
+}
+
+
+// 결국 해설 찾아보기 ㅠ
+function solution(dirs) {
+    let move = { L: [-1, 0], R: [1, 0], U: [0, 1], D: [0, -1] };
+    let now = [0, 0];
+    let route = new Set();
+    
+    for (let dir of dirs) {
+        let nowX = now[0] + move[dir][0];
+        let nowY = now[1] + move[dir][1];
+        
+        if (nowX > 5 || nowX < -5 || nowY > 5 || nowY < -5) continue;
+        
+        route.add("" + now[0] + now[1] + nowX + nowY);
+        route.add("" + nowX + nowY + now[0] + now[1]);
+        
+        now = [nowX, nowY];
+    }
+    
+    return route.size / 2;
+}
