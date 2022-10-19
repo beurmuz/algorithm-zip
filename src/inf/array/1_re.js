3ac33a735dd3194e93a0001d18dbe15954588bc0
beurmuz <fallinta2@gmail.com>
2022-10-20 08:24:31 +0900

    #2 22.10.20 > 큰 수 출력하기_re

---
+++ src/inf/array/1_re.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function solution(arr) {
+  let answer = [];
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] < arr[i]) answer.push(arr[i]); // 자신의 바로 앞 수보다 큰 수만 answer에 push
+  }
+  return answer;
+}
+
+let arr = [7, 3, 9, 5, 6, 12];
+console.log(solution(arr));
