8f2d982402b8856ec46674db87d09ef5951f360c
beurmuz <fallinta2@gmail.com>
2022-10-27 22:09:40 +0900

    #5 22.10.27 > 연속부분 수열2 (with 투포인터)

--- src/inf/twoPointer, hash_etc/4_rere.js
+++ src/inf/twoPointer, hash_etc/4_rere.js
@@ -1,16 +1,35 @@
 "use strict";
 
+// [방법 1] 이중 for문 사용하기
+// function solution(m, arr) {
+//   let answer = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     let sum = 0;
+//     for (let j = i; j < arr.length; j++) {
+//       sum += arr[j];
+//       if (sum <= m) answer++;
+//       if (sum > m) break;
+//     }
+//   }
+//   return answer;
+// }
+
+// [방법 2] 투포인터 활용하기
 function solution(m, arr) {
   let answer = 0;
-  for (let i = 0; i < arr.length; i++) {
+  let rt = 0;
+
+  for (let lt = 0; lt < arr.length; lt++) {
     let sum = 0;
-    for (let j = i; j < arr.length; j++) {
-      sum += arr[j];
-      if (sum <= m) answer++;
-      if (sum > m) break;
+    rt = lt;
+    while (sum <= m) {
+      sum += arr[rt++];
+      if (sum <= m) {
+        answer++;
+        // console.log(`sum: ${sum}, lt: ${lt}, rt: ${rt}`);
+      }
     }
   }
-
   return answer;
 }
 
