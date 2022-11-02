b14804c576fc76e5856e4353b14540f4d31df6eb
beurmuz <fallinta2@gmail.com>
2022-11-02 17:39:17 +0900

    #4 22.11.02 > 졸업 선물

--- src/inf/brute_force/4_re.js
+++ src/inf/brute_force/4_re.js
@@ -1,22 +1,28 @@
 "use strict";
 
-function solution(m, product) {
+/*
+  n: 학생수
+  total: 선생님이 가지고 있는 총 비용
+  products: 학생들이 고른 선물 리스트(선물 가격, 배송비)
+*/
+
+function solution(n, total, products) {
   let answer = 0;
-  let n = product.length;
-  product.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
+
+  // 학생들이 고른 선물 리스트(선물 가격 + 배송비의 합으로) 오름차순 정렬하기
+  products.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
   for (let i = 0; i < n; i++) {
-    let changeMoney = m - (product[i][0] / 2 + product[i][1]);
-    let cnt = 1;
+    let discountMoney = total - products[i][0] / 2 + products[i][1]; // 선물 반값 할인 가격, 배송비
+    let count = 1;
+
     for (let j = 0; j < n; j++) {
-      if (j !== i && product[j][0] + product[j][1] > changeMoney) {
-        break;
-      }
-      if (j !== i && product[j][0] + product[j][1] <= changeMoney) {
-        changeMoney -= product[j][0] + product[j][1];
-        cnt++;
+      if (j !== i && products[j][0] + products[j][1] > discountMoney) break;
+      if (j !== i && products[j][0] + products[j][1] <= discountMoney) {
+        discountMoney -= products[j][0] + products[j][1];
+        count++;
       }
     }
-    answer = Math.max(answer, cnt);
+    answer = Math.max(answer, count);
   }
   return answer;
 }
@@ -28,4 +34,4 @@ let arr = [
   [4, 5],
   [10, 3],
 ];
-console.log(solution(28, arr));
+console.log(solution(5, 28, arr));
