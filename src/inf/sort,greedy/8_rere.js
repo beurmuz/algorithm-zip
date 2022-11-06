035e3f9a35b0e221e6e8103a3bc60d4435dee8ae
beurmuz <fallinta2@gmail.com>
2022-11-06 13:21:31 +0900

    #7 22.11.06 > 회의실 배정 다시풀기

--- src/inf/sort,greedy/8_rere.js
+++ src/inf/sort,greedy/8_rere.js
@@ -3,19 +3,18 @@
 function solution(arr) {
   let answer = [];
 
+  // 회의 종료 시간 기준으로 정렬
   arr.sort((a, b) => {
-    if (a[1] === b[1]) {
-      return a[0] - b[0];
-    } else {
-      return a[1] - b[1];
-    }
+    if (a[1] === b[1]) return a[0] - b[0];
+    return a[1] - b[1];
   });
 
-  let endTime = 0;
-  for (let x of arr) {
-    if (x[0] >= endTime) {
-      answer.push(x);
-      endTime = x[1];
+  // 종료 시간보다 크거나 같은 시간만 찾기
+  let pivot = 0;
+  for (let [start, end] of arr) {
+    if (start >= pivot) {
+      answer.push([start, end]);
+      pivot = end;
     }
   }
   return answer.length;
