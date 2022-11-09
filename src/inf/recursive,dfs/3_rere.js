6d2b864a7ce0c2c5f66d2bfbbc596a04b5b9aad5
beurmuz <fallinta2@gmail.com>
2022-11-09 15:34:22 +0900

    #8 22.11.09 > dfs를 이용한 중위순회

--- src/inf/recursive,dfs/3_rere.js
+++ src/inf/recursive,dfs/3_rere.js
@@ -7,8 +7,13 @@ function solution(v, n) {
     if (v > 7) return;
     else {
       // 1. 전위 순회
-      console.log(v);
+      // console.log(v);
+      // dfs(v * 2);
+      // dfs(v * 2 + 1);
+
+      // 2. 중위 순회
       dfs(v * 2);
+      console.log(v);
       dfs(v * 2 + 1);
     }
   }
