05419aec80830b3f9a73f58ae676d5a415a605a7
beurmuz <fallinta2@gmail.com>
2022-11-14 15:49:30 +0900

    #8 22.11.14 > 동전교환 다시풀기

---
+++ src/inf/recursive,dfs/9_rerere.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function solution(arr, change) {
+  let answer = Number.MAX_SAFE_INTEGER;
+
+  function dfs(L, sum) {
+    if (sum > change) return;
+    if (L >= answer) return;
+    if (sum === change) {
+      console.log(L, sum);
+      answer = Math.min(answer, L);
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        dfs(L + 1, sum + arr[i]);
+      }
+    }
+  }
+  dfs(0, 0);
+  return answer;
+}
+
+let arr = [1, 2, 5];
+console.log(solution(arr, 15));
