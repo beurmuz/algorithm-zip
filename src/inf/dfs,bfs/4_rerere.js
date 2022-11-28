589d6858a66c0dfb39ba5fd1ca054d82dac7e6be
beurmuz <fallinta2@gmail.com>
2022-11-28 22:20:42 +0900

    #8 22.11.28 > 미로 탐색 복습

--- src/inf/dfs,bfs/4_rerere.js
+++ src/inf/dfs,bfs/4_rerere.js
@@ -4,6 +4,7 @@ function solution(board, n) {
   let dy = [0, 1, 0, -1];
 
   function dfs(x, y) {
+    // (x,y)에서 4방향씩 탐색 (가짓수가 4개씩 늘어나는 트리형태)
     if (x === n - 1 && y === n - 1) {
       answer++;
     } else {
@@ -18,8 +19,7 @@ function solution(board, n) {
           ny <= n - 1 &&
           board[nx][ny] === 0
         ) {
-          //   console.log(`(nx, ny)는 (${nx},${ny})`);
-          board[nx][ny] = 1;
+          board[nx][ny] = 1; // 1로 바꿔줘야 왔던 길을 다시 탐색하지 않음
           dfs(nx, ny);
           board[nx][ny] = 0; // back
         }
