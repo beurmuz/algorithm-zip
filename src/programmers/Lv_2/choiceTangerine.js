e87afa7b94b9adfa7188bfa92644dacab9c154ba
beurmuz <fallinta2@gmail.com>
2022-12-06 16:18:36 +0900

    #13 22.12.06 > 귤 고르기 다른 코드

--- src/programmers/Lv_2/choiceTangerine.js
+++ src/programmers/Lv_2/choiceTangerine.js
@@ -31,3 +31,17 @@ function solution(k, tangerine) {
 console.log(solution(6, [1, 3, 2, 5, 4, 5, 2, 3])); // 3
 console.log(solution(4, [1, 3, 2, 5, 4, 5, 2, 3])); // 2
 console.log(solution(2, [1, 1, 1, 1, 2, 2, 2, 3])); // 1
+
+// 2. 아주 간단한 풀이법
+function solution(k, tangerine) {
+  let answer = 0;
+  const tDict = {};
+  tangerine.forEach((t) => (tDict[t] = (tDict[t] || 0) + 1)); // 이 부분이 특히 깔끔
+  const tArr = Object.values(tDict).sort((a, b) => b - a);
+  for (const t of tArr) {
+    answer++;
+    if (k > t) k -= t;
+    else break;
+  }
+  return answer;
+}
