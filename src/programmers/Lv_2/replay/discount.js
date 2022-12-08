52df4409edb66f745a70e0c1263f520d9b1dd902
beurmuz <fallinta2@gmail.com>
2022-12-08 12:01:08 +0900

    #13 22.12.08 > 할인 행사 (with JS function) 복습

--- src/programmers/Lv_2/replay/discount.js
+++ src/programmers/Lv_2/replay/discount.js
@@ -1,35 +1,36 @@
 "use strict";
 
-function solution(want, number, discount) {
-  let answer = 0;
-  let shopping = new Map();
+// 1. using the silding window
+// function solution(want, number, discount) {
+//   let answer = 0;
+//   let shopping = new Map();
 
-  // 생성된 map과 want, number를 비교하는 function
-  function checker(tmpMap) {
-    for (let i = 0; i < want.length; i++) {
-      if (tmpMap.get(want[i]) !== number[i]) return;
-    }
-    answer++;
-  }
+//   // 생성된 map과 want, number를 비교하는 function
+//   function checker(tmpMap) {
+//     for (let i = 0; i < want.length; i++) {
+//       if (tmpMap.get(want[i]) !== number[i]) return;
+//     }
+//     answer++;
+//   }
 
-  for (let i = 0; i < 10; i++) {
-    if (shopping.has(discount[i]))
-      shopping.set(discount[i], shopping.get(discount[i]) + 1);
-    else shopping.set(discount[i], 1);
-  }
-  checker(shopping);
+//   for (let i = 0; i < 10; i++) {
+//     if (shopping.has(discount[i]))
+//       shopping.set(discount[i], shopping.get(discount[i]) + 1);
+//     else shopping.set(discount[i], 1);
+//   }
+//   checker(shopping);
 
-  // silding window
-  for (let i = 10; i <= discount.length; i++) {
-    shopping.set(discount[i - 10], shopping.get(discount[i - 10]) - 1);
-    if (shopping.has(discount[i]))
-      shopping.set(discount[i], shopping.get(discount[i]) + 1);
-    else shopping.set(discount[i], 1);
-    checker(shopping);
-  }
+//   // silding window
+//   for (let i = 10; i <= discount.length; i++) {
+//     shopping.set(discount[i - 10], shopping.get(discount[i - 10]) - 1);
+//     if (shopping.has(discount[i]))
+//       shopping.set(discount[i], shopping.get(discount[i]) + 1);
+//     else shopping.set(discount[i], 1);
+//     checker(shopping);
+//   }
 
-  return answer;
-}
+//   return answer;
+// }
 
 console.log(
   solution(
@@ -72,3 +73,21 @@ console.log(
     ]
   )
 );
+
+// 2. using the Javascript functions
+function solution(want, number, discount) {
+  let answer = 0;
+  for (let i = 0; i < discount.length - 9; i++) {
+    const cutArray = discount.slice(i, i + 10);
+
+    let flag = 1;
+    for (let j = 0; j < want.length; j++) {
+      if (cutArray.filter((item) => item === want[j]).length !== number[j]) {
+        flag = 0;
+        break;
+      }
+    }
+    if (flag) answer += 1;
+  }
+  return answer;
+}
