9fd6a6bb7b13b04affab22000acdfd230fe64df1
beurmuz <fallinta2@gmail.com>
2022-12-09 13:58:51 +0900

    #13 22.12.09 > 삼각 달팽이

---
+++ src/programmers/Lv_2/triSnail.js
@@ -0,0 +1,88 @@
+"use strict";
+
+/*
+    - 모르겠어서 풀이 참고
+    - 풀이법
+    1. 3부분으로 나눠 해당 위치의 값을 저장하기
+        - 위 -> 아래 (대각선)
+        - 왼 -> 오
+        - 아래 -> 위 (대각선)
+    2. 해당 작업을 n의 값이 유효할 때까지 반복
+*/
+
+function solution(n) {
+  let answer = [];
+  let count = 0;
+  let arr = Array.from({ length: n }, (_, index) => Array(index + 1).fill(0)); // 각 row마다 1~n개의 0으로 채우기
+
+  let nx = -1;
+  let ny = 0;
+  while (n > 0) {
+    // 위 -> 아래로 내려가는 대각선
+    for (let i = 0; i < n; i++) {
+      nx++;
+      count++;
+      arr[nx][ny] = count;
+    }
+
+    // 왼 -> 오로 이동
+    for (let i = 0; i < n; i++) {
+      nx++;
+      count++;
+      arr[nx][ny] = count;
+    }
+
+    // 아래 -> 위로 올라가는 대각선
+    for (let i = 0; i < n - 2; i++) {
+      nx--;
+      ny--;
+      count++;
+      arr[nx][ny] = count;
+    }
+    n -= 3;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    answer = [...answer, ...arr[i]];
+  }
+
+  return answer;
+}
+function solution(n) {
+  let answer = [];
+  let count = 0;
+  let arr = Array.from({ length: n }, (_, index) => Array(index + 1).fill(0));
+  let currentX = -1;
+  let currentY = 0;
+  while (n > 0) {
+    // n의 값이 유효할 때까지 반복
+    for (let i = 0; i < n; i++) {
+      // 위에서 아래로 내려가는 대각선
+      currentX++;
+      count++;
+      arr[currentX][currentY] = count;
+    }
+    for (let i = 0; i < n - 1; i++) {
+      // 왼쪽에서 오른쪽으로 이동
+      currentY++;
+      count++;
+      arr[currentX][currentY] = count;
+    }
+    for (let i = 0; i < n - 2; i++) {
+      // 아래에서 위로 내려가는 대각선
+      currentX--;
+      currentY--;
+      count++;
+      arr[currentX][currentY] = count;
+    }
+    n -= 3;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    answer = [...answer, ...arr[i]];
+  }
+
+  return answer;
+}
+
+console.log(solution(4));
