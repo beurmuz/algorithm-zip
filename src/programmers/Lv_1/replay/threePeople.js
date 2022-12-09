c2022e39e70dcea2d2843ed032bb9e018c761314
beurmuz <fallinta2@gmail.com>
2022-12-09 10:58:48 +0900

    #12 22.12.09 > 삼총사 다시풀기2

--- src/programmers/Lv_1/replay/threePeople.js
+++ src/programmers/Lv_1/replay/threePeople.js
@@ -1,21 +1,47 @@
 "use strict";
 
+// dfs 이용하기1
+// function solution(number) {
+//   let answer = 0;
+
+//   function dfs(L, combination) {
+//     if (combination.length === 3) {
+//       answer +=
+//         combination.reduce((acc, value) => acc + value, 0) === 0 ? 1 : 0;
+//       return;
+//     }
+
+//     for (let i = L; i < number.length; i++) {
+//       dfs(i + 1, [...combination, number[i]]);
+//     }
+//   }
+//   dfs(0, []);
+
+//   return answer;
+// }
+
+// 파라미터를 다르게해서 dfs 풀어보기
 function solution(number) {
   let answer = 0;
+  let checked = Array.from({ length: number.length }, () => 0);
 
-  function dfs(L, combination) {
-    if (combination.length === 3) {
-      answer +=
-        combination.reduce((acc, value) => acc + value, 0) === 0 ? 1 : 0;
-      return;
+  function dfs(L, index) {
+    if (L === 3) {
+      let sum = 0;
+      checked.map((value, idx) => {
+        if (value) sum += number[idx];
+      });
+      answer += sum === 0 ? 1 : 0;
     }
 
-    for (let i = L; i < number.length; i++) {
-      dfs(i + 1, [...combination, number[i]]);
+    for (let i = index; i < number.length; i++) {
+      if (checked[i] === 1) continue; // 1이면 바로 빠져나옴
+      checked[i] = 1;
+      dfs(L + 1, i);
+      checked[i] = 0;
     }
   }
-  dfs(0, []);
-
+  dfs(0, 0);
   return answer;
 }
 
