38db3f1e5ff97037970df0bd428361d6a012b235
beurmuz <fallinta2@gmail.com>
2022-12-11 01:00:50 +0900

    #13 22.12.11 > 삼각 달팽이 다시풀기

--- src/programmers/Lv_2/replay/triSnail.js
+++ src/programmers/Lv_2/replay/triSnail.js
@@ -4,23 +4,27 @@ function solution(n) {
   let answer = [];
   let count = 0;
   let arr = Array.from({ length: n }, (_, index) => Array(index + 1).fill(0));
+
   let currentX = -1;
   let currentY = 0;
   while (n > 0) {
     // n의 값이 유효할 때까지 반복
     for (let i = 0; i < n; i++) {
+      // 1, 2, 3, 4 -> 다음 턴에서 10이 들어옴
       // 위에서 아래로 내려가는 대각선
       currentX++;
       count++;
       arr[currentX][currentY] = count;
     }
     for (let i = 0; i < n - 1; i++) {
+      // 5, 6, 7
       // 왼쪽에서 오른쪽으로 이동
       currentY++;
       count++;
       arr[currentX][currentY] = count;
     }
     for (let i = 0; i < n - 2; i++) {
+      // 8, 9
       // 아래에서 위로 내려가는 대각선
       currentX--;
       currentY--;
@@ -36,3 +40,5 @@ function solution(n) {
 
   return answer;
 }
+
+console.log(solution(4));
