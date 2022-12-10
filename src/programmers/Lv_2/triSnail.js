1180404438c9b85877520d6e5072f8b196a08cd8
beurmuz <fallinta2@gmail.com>
2022-12-11 01:05:04 +0900

    #13 22.12.11 > 삼각 달팽이 다른 풀이

--- src/programmers/Lv_2/triSnail.js
+++ src/programmers/Lv_2/triSnail.js
@@ -48,3 +48,20 @@ function solution(n) {
 }
 
 console.log(solution(4));
+
+// 2. 다른 풀이
+function solution(n) {
+  let a = Array(n)
+    .fill()
+    .map((_, i) => Array(i + 1).fill());
+  let row = -1;
+  let col = 0;
+  let fill = 0;
+  for (let i = n; i > 0; i -= 3) {
+    a[++row][col] = ++fill;
+    for (let j = 0; j < i - 1; j++) a[++row][col] = ++fill;
+    for (let j = 0; j < i - 1; j++) a[row][++col] = ++fill;
+    for (let j = 0; j < i - 2; j++) a[--row][--col] = ++fill;
+  }
+  return a.flat();
+}
