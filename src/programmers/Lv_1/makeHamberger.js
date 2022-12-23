6208aaec453d9cd474e97e363c4b5926dc35c160
beurmuz <fallinta2@gmail.com>
2022-12-23 13:26:32 +0900

    #12 22.12.23 > 햄버거 만들기 다른 풀이

--- src/programmers/Lv_1/makeHamberger.js
+++ src/programmers/Lv_1/makeHamberger.js
@@ -1,5 +1,9 @@
 "use strict";
 
+/**
+ * 1. 처음에 푼 풀이
+ * stack을 이용해 push, pop하는 방법
+ */
 function solution(ingredient) {
   let answer = 0;
   let stack = [];
@@ -22,5 +26,26 @@ function solution(ingredient) {
       }
     }
   });
+  return answer;
+}
+
+/**
+ * 2. for문을 이용한 풀이법
+ * for문의 index를 이용해 푸는 방법
+ */
+function solution(ingredient) {
+  let answer = 0;
+
+  // ingredient 길이만큼 반복문을 돈다.
+  for (let i = 0; i < ingredient.length; i++) {
+    // i부터 i+4, 총 4개를 잘라서 join한 뒤, 그 값이 1231인지 확인한다.
+    if (ingredient.slice(i, i + 4).join("") === "1231") {
+      // 맞으면 answer를 1 증가시키고, ingredient 배열에서 해당 지점(i) 부터 총 4개를 잘라낸다.
+      answer++;
+      ingredient.splice(i, 4);
+      // 잘라냈으면 i가 다시 3개 줄어든 상태로 만들어준다.
+      i -= 3;
+    }
+  }
   return answer;
 }
