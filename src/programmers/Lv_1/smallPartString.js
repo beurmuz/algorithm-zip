df488eec1a9a5359dfa8b316f2fc8d62b2e77633
beurmuz <fallinta2@gmail.com>
2022-12-26 14:13:05 +0900

    #12 22.12.26 > 크기가 작은 부분 문자열 다시풀기

--- src/programmers/Lv_1/smallPartString.js
+++ src/programmers/Lv_1/smallPartString.js
@@ -1,23 +1,24 @@
 "use strict";
 
-// 1번 풀이
+/**
+ * 1. slice를 이용한 방법
+ */
 function solution(t, p) {
-  const numberP = Number(p);
+  const numP = p * 1;
   let count = 0;
 
   for (let i = 0; i < t.length - p.length + 1; i++) {
     const slicedT = t.slice(i, i + p.length);
-    const numberT = Number(slicedT);
-
-    if (numberT <= numberP) {
-      count += 1;
-    }
+    const numT = slicedT * 1;
+    if (numT <= numP) count++;
   }
-
   return count;
 }
 
-// 2번 풀이
+/**
+ * 2. 똑같이 slice 이용한 풀이법
+ * (코드가 조금 더 간결하다)
+ */
 function solution(t, p) {
   let count = 0;
   for (let i = 0; i <= t.length - p.length; i++) {
