b4ecd682eb72f03df94905387bc0aed5d79bfeca
beurmuz <fallinta2@gmail.com>
2022-12-27 15:38:46 +0900

    #17 22.12.27 > 4 > 기찍N

---
+++ src/bj/bronze/4/2742.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * 1. 처음에 푼 풀이
+ * - 시간초과 났다
+ */
+let n = require("fs").readFileSync("/dev/stdin").toString().trim() * 1;
+
+function solution(n) {
+  for (let i = n; i > 0; i--) {
+    console.log(i);
+  }
+}
+solution(n);
+
+/**
+ * 2. 다시 푼 풀이
+ * 변수 만들고, 문자열 형태로 다 더한 뒤에 출력했다
+ */
+let n = require("fs").readFileSync("/dev/stdin").toString().trim() * 1;
+
+function solution(n) {
+  let answer = "";
+  for (let i = n; i > 0; i--) {
+    answer += i + "\n";
+  }
+  return answer;
+}
+console.log(solution(n));
