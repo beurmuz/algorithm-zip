22303821ca32e4056795b4ad1f5cd2ff4bb8bdd3
beurmuz <fallinta2@gmail.com>
2023-01-16 11:46:35 +0900

    #18 23.01.16 > 4 > ATM 복습

--- src/bj/silver/4/replay/11399.js
+++ src/bj/silver/4/replay/11399.js
@@ -1,5 +1,8 @@
 "use strict";
 
+/**
+ * 그리디 알고리즘과 정렬을 이용하면 빠르게 풀 수 있는 문제이다.
+ */
 const [N, ...input] = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
@@ -7,13 +10,14 @@ const [N, ...input] = require("fs")
   .split("\n");
 
 const solution = (N, input) => {
-  const arr = ("" + input)
+  const arr = input
     .split(" ")
     .map((v) => +v)
-    .sort((a, b) => a - b);
+    .sort((a, b) => a - b); // 받아온 input을 정렬한 뒤
   let answer = 0;
 
   for (let i = 0; i < N; i++) {
+    // 각 사람이 돈을 인출할 때마다 걸리는 시간은 앞 사람의 인출시간을 누적합 한 것
     for (let j = 0; j <= i; j++) {
       answer += arr[j];
     }
@@ -22,3 +26,4 @@ const solution = (N, input) => {
 };
 
 console.log(solution(N, input));
+// console.log(solution(5, "3 1 4 3 2"));
