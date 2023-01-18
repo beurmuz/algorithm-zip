40604e81bad0fabaecb8593bb66539f4c8d6a8a8
beurmuz <fallinta2@gmail.com>
2023-01-18 19:17:49 +0900

    #18 23.01.18 > 1 > 회의실 배정

--- src/bj/silver/1/1931_re.js
+++ src/bj/silver/1/1931_re.js
@@ -1,28 +1,38 @@
-'use strict';
+"use strict";
 
-const inputs = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const n = inputs.shift();
-let timeTable = [];
-inputs.forEach((line) => {
-    let [start, end] = line.split(' ');
-    timeTable.push([+start, +end]);
-});
+/**
+ * 그리디 알고리즘
+ * - 그리디는 정렬을 적절히 이용해 풀면 된다!
+ */
+const [n, ...inputs] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
-timeTable.sort((a, b)=> {
-    if(a[1] === b[1]) {
-        return a[0]-b[0];
+const solution = (n, inputs) => {
+  // timeTable에 inputs값 split해서 저장하기
+  const timeTable = inputs.map((v) => {
+    let [start, end] = v.split(" ");
+    return [+start, +end];
+  });
+
+  // timeTable sort하기 (end 오름차순 => end가 같으면 start 오름차순)
+  timeTable.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    return a[1] - b[1];
+  });
+
+  const ableTime = [];
+  let endtime = 0; // 끝 시간을 0으로 초기화해놓고,
+  for (let [start, end] of timeTable) {
+    if (start >= endtime) {
+      // 만약 시작 시간이 그전 endtime보다 크거나 같으면
+      ableTime.push([start, end]); // 정답 배열에 push한다.
+      endtime = end; // endtime을 새롭게 현재 end시간으로 갱신해준다.
     }
-    return a[1]-b[1];
-});
+  }
+  return ableTime.length;
+};
 
-
-const answer = [];
-let endTime = 0;
-for(let [start, end] of timeTable) {
-    if(start >= endTime) { // 현재 시간이, 그 전 endTime보다 크거나 같다면
-        answer.push([start, end]);
-        endTime = end;
-    }
-}
-
-console.log(answer.length);
+console.log(solution(+n, inputs));
