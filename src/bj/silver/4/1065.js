65b268d64edbc3abcb0606aa81860b89bfea245b
beurmuz <fallinta2@gmail.com>
2023-02-11 15:24:49 +0900

    #18 23.02.11 > 4 > 한수 복습

--- src/bj/silver/4/1065.js
+++ src/bj/silver/4/1065.js
@@ -10,13 +10,16 @@ const solution = (n) => {
   for (let i = 1; i <= n; i++) {
     let nString = String(i);
     if (i < 100) {
+      // 100이하의 값일 때에는 각 자릿수를 비교하는 의미가 없어 모든 수가 한수가 된다.
       answer++;
       continue;
     }
     let a = +nString[0] - +nString[1]; // 자릿 수 비교
     let b = +nString[1] - +nString[2]; // 자릿 수 비교
+    // console.log(`${i}의 자릿수 비교 a: ${a},  b: ${b}`);
     if (a === b) {
       // 차이가 같으면 한수이다.
+      // console.log(`${i}는 한수 !!`);
       answer++;
     }
   }
