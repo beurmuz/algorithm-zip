79f48343d19fb1893c525df05e4f11edac96bcb9
beurmuz <fallinta2@gmail.com>
2023-03-02 08:18:46 +0900

    #12 23.03.02 > 개인정보 수집 유효기간 풀이 추가

--- src/programmers/Lv_1/privateInfo.js
+++ src/programmers/Lv_1/privateInfo.js
@@ -1,3 +1,10 @@
+"use strict";
+
+/**
+ * 재할당 가능한 변수, mutator 메서드를 사용하는 습관을 줄이기 위해
+ * const 키워드 활용, Array.prototype의 non-mutator(ex. map, filter)만 이용하고자 했다.
+ */
+
 function solution(today, terms, privacies) {
   // 0. today정보 쪼개기
   const TODAY = today.split(".").map((DATE) => +DATE);
@@ -23,6 +30,7 @@ function solution(today, terms, privacies) {
     const M = TODAY[1] - PRIVACY.startDate[1];
     const D = TODAY[2] - PRIVACY.startDate[2];
 
+    // 기준 약관 종류에 명시된 개월 수 보다 많이 지난 경우 인덱스+1을, 덜 지난 경우 X를 반환한다.
     if (Y + M > accordType.month) return ++index;
     else {
       if (Y + M === accordType.month && D >= 0) return ++index;
