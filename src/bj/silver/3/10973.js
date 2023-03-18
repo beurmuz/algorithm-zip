fdc6950fcd65489e0b80d36c608f01f86ebcb5d9
beurmuz <fallinta2@gmail.com>
2023-03-18 23:10:49 +0900

    #18 23.03.18 > 3 > 이전 순열 다시 풀기

--- src/bj/silver/3/10973.js
+++ src/bj/silver/3/10973.js
@@ -7,6 +7,10 @@ const [N, ...input] = require("fs")
   .split(/\s/)
   .map((v) => +v);
 
+/**
+ * 1. 내가 푼 풀이
+ * - dfs로 순열을 구한다.
+ */
 const solution = (N, input) => {
   const visited = Array.from({ length: N + 1 }, () => 0);
   const answer = [];
@@ -37,6 +41,42 @@ const solution = (N, input) => {
   };
   permutation(1, []);
   return answer[answer.length - 1];
+};
+
+console.log(solution(N, input));
+
+/**
+ * 2. 다른 풀이
+ */
+const [N, ...input] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(/\s/)
+  .map((v) => +v);
+
+const solution = (N, next) => {
+  let numbers = Array.from({ length: N }, (_, idx) => ++idx);
+  let sortNumbers = [...numbers].sort((a, b) => a - b); //numbers배열 오름차순 정렬
+  //next배열이 오름차순돼있다면 순열의 가장 처음 조합이므로 -1 출력
+  if (next.every((num, index) => num === sortNumbers[index])) return -1;
+  else {
+    //배열의 맨 뒤에서부터 내림차순이 깨지는 순간의 index (i) 구하기
+    let i = N - 2;
+    while (next[i] < next[i + 1]) i--;
+
+    //next[i] 뒤의 수 중에서 next[i]보다 작은 수 중에서 가장 큰 값을 가지는 index (j) 구하기
+    let j = N - 1;
+    while (next[i] < next[j]) j--;
+
+    //next[i]와 next[j] swap하기
+    [next[i], next[j]] = [next[j], next[i]];
+
+    let rest = next.slice(i + 1); //next[i] 뒤의 값들만 가지는 rest 배열 만들기
+    rest.sort((a, b) => b - a); //next[i] 뒤의 값들은 내림차순 정렬
+    let answer = [...next.slice(0, i + 1), ...rest]; //떼놨던 next[i]까지의 수와 rest합치기
+    return answer.join(" ");
+  }
 };
 
 console.log(solution(N, input));
