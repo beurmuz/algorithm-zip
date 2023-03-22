a6ff5de899f82e9f7220915769b138d71c368850
beurmuz <fallinta2@gmail.com>
2023-03-22 14:38:51 +0900

    #19 23.03.22 > 4 > 최단 경로 (Heap구현) 복습

--- src/bj/gold/4/1753.js
+++ src/bj/gold/4/1753.js
@@ -4,137 +4,105 @@
  * 다익스트라 알고리즘
  * - 우선순위큐(최소힙)으로 풀어야 한다.
  * - 그래프에 정보를 입력받고 minHeap에 [start, 0]을 push한다. => 시작 지점부터 큐에서 빠져나와하니 거리에 0을 넣는다.
- * -
+ * - 최소힙은 pop하면 거리가 가장 짧은(=가까운) 노드가 나온다.
+ * - 방문하지 않은 경우 방문체크를 하고, graph를 순회하며 현재 거리보다 작은 경우 거리를 갱신하고 힙에 추가해준다.
  */
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  empty() {
+    if (this.heap.length == 0) return true;
+    return false;
+  }
+
+  swap(arr, x, y) {
+    let tmp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = tmp;
+    return;
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let nowIndex = this.heap.length - 1; // 가장 마지막에 추가된 값의 인덱스 번호
+
+    while (nowIndex > 0) {
+      const parentIndex = Math.floor((nowIndex - 1) / 2);
+      if (this.heap[parentIndex].cost <= this.heap[nowIndex].cost) break;
+      this.swap(this.heap, parentIndex, nowIndex);
+      nowIndex = parentIndex;
+    }
+  }
+
+  extractMin() {
+    if (this.heap.length == 1) return this.heap.pop();
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.sinkDown(0);
+    return min;
+  }
+
+  sinkDown(index) {
+    const leftIndex = 2 * index + 1;
+    const rightIndex = 2 * index + 2;
+    const length = this.heap.length;
+    let smallestIndex = index;
+
+    if (
+      leftIndex < length &&
+      this.heap[leftIndex].cost < this.heap[smallestIndex].cost
+    )
+      smallestIndex = leftIndex;
+    if (
+      rightIndex < length &&
+      this.heap[rightIndex].cost < this.heap[smallestIndex].cost
+    )
+      smallestIndex = rightIndex;
+    if (smallestIndex !== index) {
+      this.swap(this.heap, index, smallestIndex);
+      this.sinkDown(smallestIndex);
+    }
+  }
+}
+
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-class minHeap {
-  heapArray = [];
-  constructor() {
-    this.heapArray.push(null);
-  }
+const solution = (input) => {
+  const [V, E] = input[0].split(" ").map((v) => +v);
+  const startpoint = +input[1];
+  const cost = Array.from({ length: V + 1 }, () => Infinity);
+  const graph = Array.from({ length: V + 1 }, () => []);
 
-  push(data) {
-    if (this.heapArray === null) {
-      this.heapArray = [];
-      this.heapArray.push(null);
-      this.heapArray.push(data);
-    } else {
-      this.heapArray.push(data);
-      let inserted_idx = this.heapArray.length - 1;
-      let parent_idx = parseInt(inserted_idx / 2);
-      while (inserted_idx > 1) {
-        if (this.heapArray[inserted_idx][1] < this.heapArray[parent_idx][1]) {
-          const tmp = this.heapArray[inserted_idx];
-          this.heapArray[inserted_idx] = this.heapArray[parent_idx];
-          this.heapArray[parent_idx] = tmp;
-          inserted_idx = parent_idx;
-          parent_idx = parseInt(parent_idx / 2);
-        } else {
-          break;
-        }
+  for (let i = 2; i < input.length; i++) {
+    const [u, v, w] = input[i].split(" ").map((v) => +v);
+    graph[u].push([v, w]); // [연결된 노드, 비용]
+  }
+  cost[startpoint] = 0;
+
+  let heap = new MinHeap();
+  heap.insert({ node: startpoint, cost: 0 });
+
+  while (!heap.empty()) {
+    let now = heap.extractMin(); // 최솟값의 index번호를 추출한다.
+    for (let i = 0; i < graph[now.node].length; i++) {
+      const [node, value] = graph[now.node][i];
+      if (cost[node] > now.cost + value) {
+        cost[node] = now.cost + value;
+        heap.insert({ node: node, cost: cost[node] });
       }
     }
   }
-  move_down(pop_idx) {
-    const left_child = pop_idx * 2;
-    const right_child = pop_idx * 2 + 1;
+  cost.shift();
+  cost.forEach((v, i) => {
+    if (v === Infinity) cost[i] = "INF";
+  });
+  return cost.join("\n");
+};
 
-    if (left_child >= this.heapArray.length) {
-      return false;
-    } else if (right_child >= this.heapArray.length) {
-      if (this.heapArray[pop_idx][1] > this.heapArray[left_child][1]) {
-        return true;
-      }
-      return false;
-    } else {
-      if (this.heapArray[left_child][1] < this.heapArray[right_child][1]) {
-        if (this.heapArray[pop_idx][1] > this.heapArray[left_child][1]) {
-          return true;
-        }
-        return false;
-      } else {
-        if (this.heapArray[pop_idx][1] > this.heapArray[right_child][1]) {
-          return true;
-        }
-        return false;
-      }
-    }
-  }
-
-  pop() {
-    if (this.heapArray === null) {
-      return null;
-    } else {
-      const return_data = this.heapArray[1];
-      this.heapArray[1] = this.heapArray[this.heapArray.length - 1];
-      this.heapArray.pop();
-      let popped_idx = 1;
-      while (this.move_down(popped_idx)) {
-        const left_child = popped_idx * 2;
-        const right_child = popped_idx * 2 + 1;
-        if (right_child >= this.heapArray.length) {
-          if (this.heapArray[popped_idx][1] > this.heapArray[left_child][1]) {
-            const tmp = this.heapArray[popped_idx];
-            this.heapArray[popped_idx] = this.heapArray[left_child];
-            this.heapArray[left_child] = tmp;
-            popped_idx = left_child;
-          }
-        } else {
-          if (this.heapArray[left_child][1] < this.heapArray[right_child][1]) {
-            if (this.heapArray[popped_idx][1] > this.heapArray[left_child][1]) {
-              const tmp = this.heapArray[popped_idx];
-              this.heapArray[popped_idx] = this.heapArray[left_child];
-              this.heapArray[left_child] = tmp;
-              popped_idx = left_child;
-            }
-          } else {
-            if (
-              this.heapArray[popped_idx][1] > this.heapArray[right_child][1]
-            ) {
-              const tmp = this.heapArray[popped_idx];
-              this.heapArray[popped_idx] = this.heapArray[right_child];
-              this.heapArray[right_child] = tmp;
-              popped_idx = right_child;
-            }
-          }
-        }
-      }
-      return return_data;
-    }
-  }
-}
-
-const [v, e] = input.shift().split(" ").map(Number);
-const start = +input.shift();
-const graph = Array.from({ length: v + 1 }, () => []);
-const distance = Array.from({ length: v + 1 }, () => Infinity);
-const visited = Array.from({ length: v + 1 }, () => false);
-const pq = new minHeap();
-
-input.forEach((i) => {
-  const [from, to, weight] = i.split(" ").map(Number);
-  graph[from].push([to, weight]);
-});
-
-distance[start] = 0;
-pq.push([start, 0]);
-
-while (pq.heapArray.length > 1) {
-  const [curNode, dist] = pq.pop();
-  if (visited[curNode]) continue;
-
-  visited[curNode] = true;
-  for (let [nextNode, nextDistance] of graph[curNode]) {
-    if (distance[nextNode] > distance[curNode] + nextDistance) {
-      distance[nextNode] = nextDistance + distance[curNode];
-      pq.push([nextNode, distance[nextNode]]);
-    }
-  }
-}
-console.log(
-  distance
-    .map((i) => (i === Infinity ? "INF" : i))
-    .slice(1)
-    .join("\n")
-);
+console.log(solution(input));
