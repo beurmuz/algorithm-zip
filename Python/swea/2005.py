b22e12acd83ebc09ec5e706474402d161dce00d1
beurmuz <fallinta2@gmail.com>
2023-04-05 11:38:59 +0900

    #23 23.04.05 > 파스칼의 삼각형

---
+++ Python/swea/2005.py
@@ -0,0 +1,11 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    print(f"#{test_case}")
+    n = int(input())
+    arr = [[1 for _ in range(i+1)] for i in range(n)]
+    for i in range(2, n):
+        for j in range(1, i):
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+    
+    for row in arr:
+        print(*row)
