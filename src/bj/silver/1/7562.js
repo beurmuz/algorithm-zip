3fa47b5f5ef247ab1c71890dcc0c0e5f6034b0e4
beurmuz <fallinta2@gmail.com>
2023-04-21 13:24:31 +0900

    #18 23.04.21 > 1 > 나이트의 이동 복습

--- src/bj/silver/1/7562.js
+++ src/bj/silver/1/7562.js
@@ -8,30 +8,38 @@ const [T, ...input] = require("fs")
 
 const solution = (T, input) => {
   let inIdx = 0;
-  const dx = [-2, -1, 1, 2, 2, 1, 1, 2];
-  const dy = [1, 2, 2, 1, -1, -2, -1, -1];
+  const dx = [2, 2, -2, -2, 1, 1, -1, -1];
+  const dy = [1, -1, 1, -1, 2, -2, 2, -2];
 
   for (let i = 0; i < T; i++) {
     let l = +input[inIdx];
     const board = Array.from({ length: l }, () => Array(l).fill(0));
-
     let [nowX, nowY] = input[inIdx + 1].split(" ").map((v) => +v);
-    let [moveX, moveY] = input[inIdx + 2].split(" ").map((v) => +v);
+    let [targetX, targetY] = input[inIdx + 2].split(" ").map((v) => +v);
 
-    const bfs = (L) => {
-      let queue = [[nowX, nowY]];
+    const bfs = () => {
+      const queue = [[nowX, nowY, 0]];
+      board[nowX][nowY] = 1;
+
       while (queue.length) {
-        let len = queue.length;
-        for (let i = 0; i, len; i++) {
-          let v = queue.shift();
-          if (v[0] === moveX && v[1] === moveY) return L;
+        let [x, y, count] = queue.shift();
+        if (x === targetX && y === targetY) return count;
+
+        for (let k = 0; k < dx.length; k++) {
+          let nx = x + dx[k];
+          let ny = y + dy[k];
+
+          if (nx < 0 || nx >= l || ny < 0 || ny >= l) continue;
+          if (board[nx][ny] === 0) {
+            board[nx][ny] = 1; // 방문 표시
+            queue.push([nx, ny, count + 1]);
+          }
         }
       }
     };
-    bfs(0);
-
+    console.log(bfs());
     inIdx += 3;
   }
 };
 
-console.log(solution(+T, input));
+solution(+T, input);
