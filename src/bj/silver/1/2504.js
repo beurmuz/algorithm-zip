b2eef0bbfd1ba6ce74aaefdaacf2506a812d897b
beurmuz <fallinta2@gmail.com>
2023-04-23 21:11:30 +0900

    #18 23.04.23 > 1 > 괄호의 값

--- src/bj/silver/1/2504.js
+++ src/bj/silver/1/2504.js
@@ -1,1 +1,57 @@
 "use strict";
+
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("");
+
+const checkPossible = (string) => {
+  const stack = [];
+  for (let i = 0; i < string.length; i++) {
+    let top = stack[stack.length - 1];
+    const now = string[i];
+
+    if (now === "]" && top === "[") stack.pop();
+    else if (now === ")" && top === "(") stack.pop();
+    else stack.push(now);
+  }
+  return stack.length ? false : true;
+};
+
+const solution = (input) => {
+  const stack = [];
+
+  if (!checkPossible(input)) {
+    return 0;
+  }
+
+  for (let i = 0; i < input.length; i++) {
+    let top = stack[stack.length - 1];
+    const now = input[i];
+
+    if (now === "(" || now === "[") stack.push(now);
+    else if (now === ")" || now === "]") {
+      const reverse = now === ")" ? "(" : "[";
+      const point = reverse === "(" ? 2 : 3;
+
+      if (top === reverse) {
+        stack.pop();
+        stack.push(point);
+      } else {
+        let tmp = 0;
+        while (1) {
+          const pop = stack.pop();
+          if (typeof pop === "number") tmp += pop;
+          else if (pop === reverse) {
+            stack.push(tmp * point);
+            break;
+          }
+        }
+      }
+    }
+  }
+  return stack.reduce((acc, cur) => acc + cur);
+};
+
+console.log(solution(input));
