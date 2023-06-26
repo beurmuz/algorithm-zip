4ac411c0b3bb47f75c1b83d929dd79c7be3294ea
beurmuz <fallinta2@gmail.com>
2023-06-26 12:20:36 +0900

    #18 23.06.26 > 1 > 겹치는 건 싫어 복습

--- src/bj/silver/1/20922.js
+++ src/bj/silver/1/20922.js
@@ -1,5 +1,11 @@
 "use strict";
 
+/**
+ * [투 포인터 문제]
+ * - 문제 풀이법을 제대로 이해하지 못했다.
+ * - https://tesseractjh.tistory.com/297
+ * - i와 j를 0에 둔 채 j를 증가시키면서 countMap에 각 숫자들이 등장한 횟수를 갱신한다.
+ */
 const [N, K, ...arr] = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
@@ -8,18 +14,18 @@ const [N, K, ...arr] = require("fs")
   .map((v) => +v);
 
 const solution = (N, K, arr) => {
-  const intMap = {};
+  const countMap = {};
   let maxLength = 0;
   let i = 0;
   let j = 0;
 
-  while (i <= j && j < N) {
-    while (intMap[arr[j]] === K) {
-      intMap[arr[i]]--;
+  while (j >= i && j < N) {
+    while (countMap[arr[j]] === K) {
+      countMap[arr[i]]--;
       i++;
     }
     maxLength = Math.max(maxLength, j - i + 1);
-    intMap[arr[j]] = (intMap[arr[j]] ?? 0) + 1;
+    countMap[arr[j]] = (countMap[arr[j]] ?? 0) + 1;
     j++;
   }
 
