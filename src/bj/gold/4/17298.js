c356453ab41ca6be23dbd2777b10f9e6cc8613cf
beurmuz <fallinta2@gmail.com>
2023-06-30 17:19:05 +0900

    #19 23.06.30 > 4 > 오큰수

---
+++ src/bj/gold/4/17298.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * [stack 문제]
+ */
+const [N, ...arr] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(/\s/)
+  .map((v) => +v);
+
+const solution = (N, arr) => {
+  let stack = [];
+  for (let i = 0; i < N; i++) {
+    while (stack.length && arr[stack[stack.length - 1]] < arr[i]) {
+      arr[stack.pop()] = arr[i];
+    }
+    stack.push(i);
+  }
+  while (stack.length) {
+    arr[stack.pop()] = -1;
+  }
+  return arr.join(" ");
+};
+
+console.log(solution(N, arr));
