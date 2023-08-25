c108cffc573b701b9bd3ac11bf81e1ff8bb4f9b0
beurmuz <fallinta2@gmail.com>
2023-08-25 21:17:57 +0900

    #18 23.08.25 > 3 > N과 M(3)

---
+++ src/bj/silver/3/15651.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/**
+ * [백트래킹 문제]
+ * - 중복을 허용하므로 visited는 필요없고, 반복문은 1부터 계속 돌아야한다. 그래야 1 1, 2 2, 3 3, 4 4가 나올 수 있다.
+ */
+
+const [N, M] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((v) => +v);
+
+const solution = (N, M) => {
+  let answer = [];
+  //   const visited = Array.from({ length: N + 1 }, () => false);
+
+  const dfs = (i, arr) => {
+    if (arr.length === M) {
+      answer.push(arr.join(" "));
+      return;
+    } else {
+      for (let k = 1; k <= N; k++) {
+        arr.push(k);
+        // visited[k] = true;
+        dfs(k + 1, arr);
+        // visited[k] = false;
+        arr.pop();
+      }
+    }
+  };
+  dfs(1, []);
+
+  return answer.join("\n");
+};
+
+console.log(solution(N, M));
