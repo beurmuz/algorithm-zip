b7a11675e7bf38200d32c258616f026a9012fd33
beurmuz <fallinta2@gmail.com>
2023-09-15 21:23:14 +0900

    #19 23.09.15 > 3 > 소문난 칠공주 다시풀기

--- src/bj/gold/3/1941.js
+++ src/bj/gold/3/1941.js
@@ -5,21 +5,22 @@ const input = require("fs")
   .toString()
   .trim()
   .split("\n");
-const seat = input.map((el) => el.split("")).flat(); // 2차원을 1차원 배열로 만들어준다.
 
-const visited = Array.from(Array(5), () => new Array(5).fill(0));
-const seated = Array.from(Array(5), () => new Array(5).fill(0));
-const moveX = [1, 0, -1, 0];
-const moveY = [0, 1, 0, -1];
+const N = 5;
+const seat = input.map((line) => line.split("")).flat(); // 2 -> 1차원 배열로 만들기
+const visited = Array.from({ length: N }, () => Array(N).fill(0));
+const seated = Array.from({ length: N }, () => Array(N).fill(0));
+const dx = [1, 0, -1, 0]; // 남, 동, 북, 서
+const dy = [0, 1, 0, -1];
 
 let startY;
 let startX;
 
 const queue = [];
 
-const calcY = (num) => Math.floor(num / 5);
-const calcX = (num) => num % 5;
-const getYCnt = (num) => (seat[num] === "Y" ? 1 : 0);
+const calcY = (num) => Math.floor(num / 5); // Y
+const calcX = (num) => num % 5; // X
+const getYCnt = (num) => (seat[num] === "Y" ? 1 : 0); // 'Y'의 개수를 세는 함수
 
 const checkConnectCnt = (y, x) => {
   let ret = 1;
@@ -27,13 +28,14 @@ const checkConnectCnt = (y, x) => {
   visited[y][x] = 1;
   queue.push([y, x]);
 
+  // 4방향 탐색
   for (let dir = 0; dir < 4; dir++) {
-    let ny = y + moveY[dir];
-    let nx = x + moveX[dir];
+    let ny = y + dy[dir];
+    let nx = x + dx[dir];
 
-    if (ny < 0 || ny >= 5 || nx < 0 || nx >= 5) continue;
+    if (ny < 0 || ny >= 5 || nx < 0 || nx >= 5) continue; // 범위를 넘으면 건너뛴다.
 
-    if (!seated[ny][nx] || visited[ny][nx]) continue;
+    if (!seated[ny][nx] || visited[ny][nx]) continue; // 아직 앉지않았거나, 이미 방문했다면 건너뛴다.
 
     ret += checkConnectCnt(ny, nx);
   }
@@ -42,16 +44,19 @@ const checkConnectCnt = (y, x) => {
 };
 
 const solve = (depth, start, yCount) => {
-  if (yCount >= 4) return 0;
+  // 깊이, 시작점, 임도연파의 수
+  if (yCount >= 4) return 0; //임도연파가 4명 이상이라는 것은, 이다솜 파가 4명 미만인 것이므로 정답이 될 수 없다.
 
   if (depth === 7) {
-    let ret = checkConnectCnt(startY, startX) === 7 ? 1 : 0;
+    // 깊이가 7일때
+    let ret = checkConnectCnt(startY, startX) === 7 ? 1 : 0; // 연결된 칸의 개수가 7개이면 1을, 아니면 0을 저장한다.
 
     while (queue.length) {
+      // queue가 있는 동안
       let [y, x] = queue[0];
 
       queue.shift();
-      visited[y][x] = 0;
+      visited[y][x] = 0; // 방문을 해제한다.
     }
 
     return ret;
@@ -60,6 +65,7 @@ const solve = (depth, start, yCount) => {
   let ret = 0;
 
   for (let i = start; i < 25; i++) {
+    // 총 25번 반복한다.
     const y = calcY(i);
     const x = calcX(i);
 
@@ -74,12 +80,12 @@ const solve = (depth, start, yCount) => {
 let ans = 0;
 
 for (let i = 0; i <= 18; i++) {
-  startY = calcY(i);
-  startX = calcX(i);
+  startY = calcY(i); // 시작 row
+  startX = calcX(i); // 시작 col
 
-  seated[startY][startX] = 1;
+  seated[startY][startX] = 1; // 앉음 표시를 하고
   ans += solve(1, i + 1, getYCnt(i));
-  seated[startY][startX] = 0;
+  seated[startY][startX] = 0; // 백트래킹
 }
 
 console.log(ans);
