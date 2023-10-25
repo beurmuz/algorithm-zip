ca24ca9aa43f78d7612fdd3bfb8e07abd02fd683
beurmuz <fallinta2@gmail.com>
2023-10-26 00:46:31 +0900

    #12 23.10.26 > 삼총사 다시풀기

--- src/programmers/Lv_1/replay/threePeople.js
+++ src/programmers/Lv_1/replay/threePeople.js
@@ -1,50 +1,30 @@
-"use strict";
+/**
+ * [dfs, 백트래킹 문제]
+ * - 엄청 오랜만에 푸는 유형이다. L은 레벨을 말한다. L이 3이라는 것은, 이미 3의 번호를 더했다는 것이다.
+ * - 이미 카운트한 학생은 또 카운트할 수 없으므로 체크 표시를 통해 구분지어야하며, 같은 번호를 또 고를 순 없으므로 반복문의 시작점을 인자로 받아온 index로 지정해주어야 한다.
+ */
 
-// dfs 이용하기1
-// function solution(number) {
-//   let answer = 0;
-
-//   function dfs(L, combination) {
-//     if (combination.length === 3) {
-//       answer +=
-//         combination.reduce((acc, value) => acc + value, 0) === 0 ? 1 : 0;
-//       return;
-//     }
-
-//     for (let i = L; i < number.length; i++) {
-//       dfs(i + 1, [...combination, number[i]]);
-//     }
-//   }
-//   dfs(0, []);
-
-//   return answer;
-// }
-
-// 파라미터를 다르게해서 dfs 풀어보기
 function solution(number) {
   let answer = 0;
-  let checked = Array.from({ length: number.length }, () => 0);
+  let checked = Array.from({ length: number.length }, () => false); // 방문여부 표시
 
   function dfs(L, index) {
     if (L === 3) {
       let sum = 0;
-      checked.map((value, idx) => {
-        if (value) sum += number[idx];
-      });
+      for (let j = 0; j < checked.length; j++) {
+        if (checked[j]) sum += number[j];
+      }
       answer += sum === 0 ? 1 : 0;
+      return;
     }
 
-    for (let i = index; i < number.length; i++) {
-      if (checked[i] === 1) continue; // 1이면 바로 빠져나옴
-      checked[i] = 1;
+    for (let i = index; i <= number.length; i++) {
+      if (checked[i]) continue;
+      checked[i] = true;
       dfs(L + 1, i);
-      checked[i] = 0;
+      checked[i] = false;
     }
   }
   dfs(0, 0);
   return answer;
 }
-
-console.log(solution([-2, 3, 0, 2, -5]));
-console.log(solution([-3, -2, -1, 0, 1, 2, 3]));
-console.log(solution([-1, 1, -1, 1]));
