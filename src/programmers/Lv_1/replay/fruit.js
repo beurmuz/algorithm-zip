55738d8792689207bc3179011641fb3d2f14c294
beurmuz <fallinta2@gmail.com>
2023-10-26 10:40:58 +0900

    #12 23.10.26 > 과일장수 다시풀기

--- src/programmers/Lv_1/replay/fruit.js
+++ src/programmers/Lv_1/replay/fruit.js
@@ -1,16 +1,15 @@
-"use strict";
+/**
+ * [정렬, 구현]
+ */
 
 function solution(k, m, score) {
   let answer = 0;
   score.sort((a, b) => b - a);
 
-  let index = m - 1;
+  let idx = m - 1;
   for (let i = 0; i < Math.floor(score.length / m); i++) {
-    answer += score[index] * m;
-    index += m;
+    answer += score[idx] * m;
+    idx += m;
   }
   return answer;
 }
-
-console.log(solution(3, 4, [1, 2, 3, 1, 2, 3, 1]));
-console.log(solution(4, 3, [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]));
