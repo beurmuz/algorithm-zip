04d21b6d9d83f068837bb9dd49afd812027ee5e1
beurmuz <fallinta2@gmail.com>
2023-10-27 19:04:36 +0900

    #13 23.10.27 > 전화번호 목록(다른 풀이)

--- src/programmers/Lv_2/telList.js
+++ src/programmers/Lv_2/telList.js
@@ -15,3 +15,13 @@ function solution(phone_book) {
   }
   return answer;
 }
+
+// 다른 풀이
+// startWith을 사용한 방법이다.
+function solution(phoneBook) {
+  return !phoneBook.sort().some((t, i) => {
+    if (i === phoneBook.length - 1) return false;
+
+    return phoneBook[i + 1].startsWith(phoneBook[i]);
+  });
+}
