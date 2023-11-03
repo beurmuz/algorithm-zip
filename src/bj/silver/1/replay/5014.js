7cf0249fef6892997b8f3a96d25378b6495811ed
beurmuz <fallinta2@gmail.com>
2023-11-03 23:27:09 +0900

    #18 23.11.03 > 1 > 스타트링크 다시풀기

--- src/bj/silver/1/replay/5014.js
+++ src/bj/silver/1/replay/5014.js
@@ -3,6 +3,7 @@
 /**
  * [bfs 문제]
  * - visited를 queue에 push할때 방문체크를 바로 해주었더니, 어제 짰던 코드보다 시간이 2배, 메모리도 2배 더 사용됐다.
+ * - '적어도 몇번~?', '최소한으로~' 하면 BFS로 풀면 된다.
  */
 const [F, S, G, U, D] = require("fs")
   .readFileSync("/dev/stdin")
@@ -12,20 +13,22 @@ const [F, S, G, U, D] = require("fs")
   .map((v) => +v);
 
 const solution = (F, S, G, U, D) => {
-  // 층수가 정해져있으므로 visited 배열의 범위는 F+1층까지로 한다.
+  // 총 F층. 방문 여부를 기록할 배열 선언
   const visited = Array.from({ length: F + 1 }, () => false);
 
-  // 현재 층수가 목적지와 같으면 바로 종료한다.
-  if (S === G) return 0;
+  if (S === G) return 0; // 현재 위치가 목적지라면 0을 return한다.
 
   const bfs = () => {
-    const queue = [[S, 0]]; // [시작점, 버튼을 누른 횟수]
+    let queue = [];
+    queue.push([S, 0]); // [시작 층수, 버튼을 누른 횟수]
+    visited[S] = true; // 방문 표시
 
     while (queue.length) {
-      const [now, count] = queue.shift();
-      if (now === G) return count; // 현재 지점이 G라면 탐색을 종료한다.
+      let [nowFloor, count] = queue.shift();
+      if (nowFloor === G) return count; // 현재 층수가 G와 같다면, count를 return 한다.
 
-      for (let nx of [now + U, now - D]) {
+      for (let nx of [nowFloor + U, nowFloor - D]) {
+        // 최고층이 F이므로 nx는 1층부터 F 사이에 존재해야 한다.
         if (!visited[nx] && nx > 0 && nx <= F) {
           queue.push([nx, count + 1]);
           visited[nx] = true;
@@ -33,7 +36,6 @@ const solution = (F, S, G, U, D) => {
       }
     }
   };
-  visited[S] = true;
   let answer = bfs();
   return answer ? answer : "use the stairs";
 };
