150c7a1ee8b43ea01473473e2d0560763c127024
beurmuz <fallinta2@gmail.com>
2023-11-03 21:34:19 +0900

    #18 23.11.03 > 3 > 바이러스 다시풀기

--- src/bj/silver/3/replay/2606.js
+++ src/bj/silver/3/replay/2606.js
@@ -19,30 +19,30 @@ const solution = (n, m, inputs) => {
     map[y][x] = 1;
   }
 
-  // 2. 방문 여부를 저장할 배열 생성, count 변수 생성
-  let count = 0;
-  const visited = Array.from({ length: n + 1 }, () => 0);
+  // 2. 방문 여부를 저장할 배열 생성, 정답 개수를 셀 count 변수 생성
+  const visited = Array.from({ length: n + 1 }, () => false);
 
-  // 3. 깊이우선 탐색으로 시작!
-  const dfs = (n, start) => {
-    // n은 컴퓨터 개수, start는 탐색 시작점
-    visited[start] = 1; // 방문 표시
+  // 3. dfs(깊이우선 탐색) 시작!
+  const dfs = (start) => {
+    visited[start] = true; // 방문 표시
 
     for (let i = 0; i <= n; i++) {
-      if (map[start][i] === 1 && visited[i] === 0) {
-        // start랑 연결된 지점의 값이 1이고, 그 지점을 아직 방문하지 않았다면
-        dfs(n, i);
+      if (map[start][i] === 1 && visited[i] === false) {
+        // start랑 연결되어 있고, 아직 방문하지 않았다면
+        dfs(i);
       }
     }
   };
 
-  // 4. 1번 노드와 연결된 지점 찾기!
-  dfs(n, 1);
+  // 4. 1번 노드와 연결된 지점 찾기
+  dfs(1);
 
-  // 5. 1번 노드와 관련된 노드들의 개수 구하기
+  // 5. 전염된 컴퓨터 수 구하기
+  let count = 0;
   for (let i = 2; i <= n; i++) {
-    if (visited[i] === 1) count++; // visited[i]가 1이라는 것은 방문했다는 의미이므로 count를 증가시킨다.
+    if (visited[i]) count++;
   }
+
   return count;
 };
 
