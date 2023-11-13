6d90944c65ed0b14549519fc2366750f31a2c8c4
beurmuz <fallinta2@gmail.com>
2023-11-13 22:09:17 +0900

    #17 23.11.13 > 1 > 암호 키

---
+++ src/bj/bronze/1/1816.py
@@ -0,0 +1,13 @@
+# 우우웅
+
+n = int(input())
+
+for _ in range(n):
+    num = int(input())
+    for div_num in range(2, 1000000 + 1):
+        if num % div_num == 0:
+            print("NO")
+            break
+
+        if div_num == 1000000:
+            print("YES")
