07f16a4a134c53b097222b97a5a99a6759435b4f
beurmuz <fallinta2@gmail.com>
2023-12-19 17:37:34 +0900

    #13 23.12.18 > 우박수열 정적분 풀이 추가

--- src/programmers/Lv_2/replay/math.js
+++ src/programmers/Lv_2/replay/math.js
@@ -1,11 +1,12 @@
-const solution = (k, ranges) => {
-  let graph = [];
-  let area = [0]; // 넓이를 저장
+function solution(k, ranges) {
+  const point = [];
+  const area = [0];
 
+  // k가 1이 될 때까지 순회
   while (k !== 1) {
     if (k % 2) {
-      // 각 구간의 넓이는 사다리꼴의 넓이 (윗변+아랫변)/2, ([idx+1]+[idx])/2
-      area.push((k + k * 3 + 1) / 2 + area.flat(-1));
+      // 각 구간의 넓이는 사다리꼴의 넓이 (윗변+아랫변)/2, ([idx+1]+[idx])/2 를 기록함
+      area.push((k + k * 3 + 1) / 2 + area.at(-1));
       k = k * 3 + 1;
     } else {
       area.push((k + k / 2) / 2 + area.at(-1));
@@ -13,8 +14,11 @@ const solution = (k, ranges) => {
     }
   }
 
+  // 정적분 구하기
   return ranges.map(([s, e]) => {
+    // 범위가 끝나는 지점이 시작 지점보다 뒤에 있는 경우 -1 반환
     if (area.length - 1 + e < s) return -1;
+    // 끝 범위 - 시작 범위를 통해 범위 사이의 넓이를 확인
     return area.at(e - 1) - area[s];
   });
-};
+}
