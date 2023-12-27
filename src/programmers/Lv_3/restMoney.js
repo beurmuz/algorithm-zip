f576d97bd32abdf07264985a2c37f1a711da2713
beurmuz <fallinta2@gmail.com>
2023-12-27 12:09:20 +0900

    #14 23.12.27 > 거스름돈 재풀이

--- src/programmers/Lv_3/restMoney.js
+++ src/programmers/Lv_3/restMoney.js
@@ -2,21 +2,20 @@
  * [dp, 동적프로그래밍 문제]
  * - 점화식을 찾는게 많이 어려워, 풀이를 검색해보았다.
  * - 거스름돈을 주는 방법을 구하는 것이다.
+ * - 제출하니 오답이라고 나와서 다시 풀었다.
  */
 
 function solution(n, money) {
-  const dp = Array.from({ length: n + 1 }, () => 0);
-  dp[0] = 1;
+  let dp = [1, ...Array(n + 1).fill(0)];
 
   // 가지고 있는 화폐의 개수만큼 반복하기
-  for (let i = 0; i < money.length; i++) {
-    // nowMoney는 현재 거스름돈 (0원 ~ n원)
-    for (let nowMoney = 0; nowMoney <= n; nowMoney++) {
+  for (let m of money) {
+    // i는 현재 거스름돈 (0원 ~ n원)
+    for (let i = m; i <= n; i++) {
       // 만약 현재 거스름돈이 새롭게 추가된 화폐 금액과 같거나 크다면,
       // 새롭게 추가되는 화폐를 사용할 수 있으므로 점화식을 활용해 dp 배열을 구한다.
-      if (nowMoney >= money[i]) dp[j] += dp[j - money[i]] & 1000000007;
+      dp[i] += dp[i - m] % 1000000007;
     }
   }
-
   return dp[n];
 }
