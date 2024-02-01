b357b091e84aed540d2d14838749dfe5a9a439c5
beurmuz <fallinta2@gmail.com>
2024-02-01 15:31:10 +0900

    #14 24.02.01 > 입국 심사

--- src/programmers/Lv_3/enterTest.js
+++ src/programmers/Lv_3/enterTest.js
@@ -3,25 +3,29 @@
  */
 
 function solution(n, times) {
+  // 이분 탐색을 하려면 우선 정렬을 해야한다.
   times.sort((a, b) => a - b);
-  let left = 1;
-  let right = n * times[times.length - 1];
-  let answer = right; // 최대값.
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
+
+  // 최솟값, 최댓값을 설정 (최댓값은 가장 긴 심사시간 * 대기자 수)
+  let [lt, rt] = [1, n * times[times.length - 1]];
+  let answer = rt; // answer는 rt값으로 갱신해놓는다. (= 최대 시간)
+
+  // 최소 시간 찾기 시작!
+  while (lt <= rt) {
+    let mid = Math.floor((lt + rt) / 2);
     let count = 0;
-    times.forEach((value) => {
-      count += Math.floor(mid / value); // 한 사람당 몇명 할 수 있는지
+
+    // times를 순회하면서 한 사람당 몇명을 심사할 수 있는지 찾는다.
+    times.forEach((time) => {
+      count += Math.floor(mid / time);
       if (count >= n) {
-        answer = Math.min(mid, answer); // 최솟값
+        answer = Math.min(mid, answer); // 최솟값 갱신
         return;
       }
     });
-    if (count >= n) {
-      right = mid - 1;
-    } else {
-      left = mid + 1;
-    }
+
+    if (count >= n) rt = mid - 1;
+    else lt = mid + 1;
   }
   return answer;
 }
