537d917ebc06f540c2c9edd3d0785910acf52e23
beurmuz <fallinta2@gmail.com>
2024-02-18 23:08:17 +0900

    #14 24.02.18 > 표 편집

---
+++ src/programmers/Lv_3/editTable.js
@@ -0,0 +1,76 @@
+function solution(n, k, cmd) {
+  const Node = function (index, prev) {
+    this.index = index;
+    this.prev = prev;
+    this.next = null;
+  };
+
+  let prevNode = new Node(0);
+  let select; //선택된 노드
+
+  // 링크드리스트 생성
+  for (let i = 1; i < n; i++) {
+    const cntNode = new Node(i, prevNode);
+    prevNode.next = cntNode;
+    prevNode = cntNode;
+
+    // 처음 선택된 노드 저장
+    if (i === k) {
+      select = cntNode;
+    }
+  }
+
+  let trashBin = [];
+
+  const moveSelectedNode = (count, direction) => {
+    for (let i = 0; i < count; i++) {
+      if (!select[direction]) break;
+      select = select[direction];
+    }
+  };
+
+  const deleteNode = () => {
+    const prev = select.prev;
+    const next = select.next;
+
+    trashBin.push(select);
+
+    select = next ? next : prev;
+
+    if (prev) prev.next = next;
+    if (next) next.prev = prev;
+  };
+
+  const recoverNode = () => {
+    const targetNode = trashBin.pop();
+
+    const prev = targetNode.prev;
+    const next = targetNode.next;
+
+    if (prev) prev.next = targetNode;
+    if (next) next.prev = targetNode;
+  };
+
+  cmd.forEach((c) => {
+    switch (c[0]) {
+      case "U":
+        moveSelectedNode(c.split(" ")[1], "prev");
+        break;
+      case "D":
+        moveSelectedNode(c.split(" ")[1], "next");
+        break;
+      case "C":
+        deleteNode();
+        break;
+      case "Z":
+        recoverNode();
+        break;
+    }
+  });
+
+  let result = Array(n).fill("O");
+  trashBin.forEach((node) => {
+    result[node.index] = "X";
+  });
+  return result.join("");
+}
