88426072474dca4ece75376d74aaf64c8ae003f8
beurmuz <fallinta2@gmail.com>
2024-05-20 08:46:38 +0900

    #30 2 > 24.05.20 > [함수] 변수의 영역- 2문제

--- src/tree/Lv_2/함수/변수의 영역.js
+++ src/tree/Lv_2/함수/변수의 영역.js
@@ -11,32 +11,80 @@ let [a, b] = require("fs")
   .split("\n");
 
 function matchStr() {
-    for(let i = 0; i < a.length; i++) {
-        let cnt = 0;
-        if(a[i] === b[0]) {
-            for(let j = 0; j < b.length; j++) {
-                if(a[i + j] === b[j]) cnt++;
-            }
-            if(cnt === b.length) {
-                return i;
-            }
-        }
+  for (let i = 0; i < a.length; i++) {
+    let cnt = 0;
+    if (a[i] === b[0]) {
+      for (let j = 0; j < b.length; j++) {
+        if (a[i + j] === b[j]) cnt++;
+      }
+      if (cnt === b.length) {
+        return i;
+      }
     }
-    return -1;
+  }
+  return -1;
 }
 
 console.log(matchStr());
 
 // ----------------------------------------------------------------------
 /**
- * 🔍 두 정수에 대한 연산값2 | O | 24.05.20 🔍
+ * 🔍 나누고 빼면서 합하기 | O | 24.05.20 🔍
  *
  * [함수]
  */
+const inputs = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let [n, m] = inputs[0].split(" ").map((v) => +v);
+let orders = inputs[1].split(" ").map((v) => +v);
+
+function isOdd() {
+  m = m - 1;
+}
+
+function isEven() {
+  m = parseInt(m / 2);
+}
+
+let answer = 0;
+while (m >= 1) {
+  // A의 m번째 원소 더하기
+  answer += orders[m - 1];
+  if (m === 1) break;
+  if (m % 2 === 0) isEven();
+  else isOdd();
+}
+
+console.log(answer);
 
 // ----------------------------------------------------------------------
 /**
- * 🔍 두 정수에 대한 연산값2 | O | 24.05.20 🔍
+ * 🔍 특정 구간의 합 | O | 24.05.20 🔍
  *
  * [함수]
  */
+const inputs = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, m] = inputs[0].split(" ").map((v) => +v);
+const arr = inputs[1].split(" ").map((v) => +v);
+
+function sectionSum(a, b) {
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+for (let i = 2; i < inputs.length; i++) {
+  let [a, b] = inputs[i].split(" ").map((v) => +v);
+  console.log(sectionSum(a - 1, b - 1));
+}
