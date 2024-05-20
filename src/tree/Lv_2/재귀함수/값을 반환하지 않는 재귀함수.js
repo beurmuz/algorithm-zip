2f4f46f28512d5fdeba6c8156efd3c551e1edb3d
beurmuz <fallinta2@gmail.com>
2024-05-20 14:31:34 +0900

    #30 2 > 24.05.20 > [재귀함수] 값을 반환하는 재귀함수 - 4문제

---
+++ src/tree/Lv_2/재귀함수/값을 반환하지 않는 재귀함수.js
@@ -0,0 +1,102 @@
+// ----------------------------------------------------------------------
+/**
+ * 🔍 반복 출력하기2 | O | 24.05.20 🔍
+ *
+ * [함수]
+ */
+const N = Number(require("fs").readFileSync("/dev/stdin").toString().trim());
+
+function recursive(n) {
+  if (n === 0) return;
+  recursive(n - 1); // 재호출
+  console.log("HelloWorld");
+}
+
+recursive(N);
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 숫자 차례로 출력하기 | O | 24.05.20 🔍
+ *
+ * [함수]
+ */
+const N = Number(require("fs").readFileSync("/dev/stdin").toString().trim());
+
+function asc(n) {
+  if (n === 0) return;
+  asc(n - 1);
+  process.stdout.write(n + " ");
+}
+
+function desc(n) {
+  if (n === 0) return;
+  process.stdout.write(n + " ");
+  desc(n - 1);
+}
+
+asc(N);
+console.log();
+desc(N);
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 재귀함수를 이용한 별 출력 | O | 24.05.20 🔍
+ *
+ * [함수]
+ */
+const N = Number(require("fs").readFileSync("/dev/stdin").toString().trim());
+
+function recursive(n) {
+  if (n === 0) return;
+  recursive(n - 1);
+  for (let i = 0; i < n; i++) {
+    process.stdout.write("*");
+  }
+  console.log();
+}
+
+recursive(N);
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 재귀함수의 꽃 | O | 24.05.20 🔍
+ *
+ * [함수]
+ */
+const N = Number(require("fs").readFileSync("/dev/stdin").toString().trim());
+
+let answer = "";
+function flower(n) {
+  if (n === 0) return;
+  answer += n + " ";
+  flower(n - 1);
+  answer += n + " ";
+}
+
+flower(N);
+console.log(answer);
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 재귀함수를 이용한 별 출력 | O | 24.05.20 🔍
+ *
+ * [함수]
+ */
+const N = Number(require("fs").readFileSync("/dev/stdin").toString().trim());
+
+function makeStart(n) {
+  if (n === 0) return;
+
+  let line = "";
+  for (let i = 0; i < n; i++) {
+    line += "*" + " ";
+  }
+  console.log(line);
+  makeStart(n - 1);
+  line = "";
+  for (let i = 0; i < n; i++) {
+    line += "*" + " ";
+  }
+  console.log(line);
+}
+makeStart(N);
