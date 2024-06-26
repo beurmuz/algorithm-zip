02c35b630b1e89d9edc86e186d3afb3fc8ea714b
beurmuz <fallinta2@gmail.com>
2024-06-26 20:37:19 +0900

    #30 2 > 24.06.26 > [완전탐색] 물체 단위로 완전탐색 - 1문제

--- src/tree/Lv_2/완전탐색2/물체 단위로 완전탐색.js
+++ src/tree/Lv_2/완전탐색2/물체 단위로 완전탐색.js
@@ -61,3 +61,37 @@ for (let i = 0; i < N; i++) {
 }
 
 console.log(answer);
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 삼각형 만들기 | X | 24.06.26
+ *
+ * [완전탐색2 - 물체 단위로 완전탐색]
+ */
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const points = input.slice(1, 1 + n).map((line) => line.split(" ").map(Number));
+
+function area(x1, y1, x2, y2, x3, y3) {
+  return Math.abs(x1 * y2 + x2 * y3 + x3 * y1 - (x2 * y1 + x3 * y2 + x1 * y3));
+}
+
+let maxArea = 0;
+for (let i = 0; i < n; i++) {
+  for (let j = i + 1; j < n; j++) {
+    for (let k = j + 1; k < n; k++) {
+      const [x1, y1] = points[i];
+      const [x2, y2] = points[j];
+      const [x3, y3] = points[k];
+      if (
+        (x1 === x2 || x1 === x3 || x2 === x3) &&
+        (y1 === y2 || y1 === y3 || y2 === y3)
+      ) {
+        maxArea = Math.max(maxArea, area(x1, y1, x2, y2, x3, y3));
+      }
+    }
+  }
+}
+
+console.log(maxArea);
