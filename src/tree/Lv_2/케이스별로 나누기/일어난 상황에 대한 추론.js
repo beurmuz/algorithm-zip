d3a7f3957363dadd7ead3c4596c08c7c85ff03b8
beurmuz <fallinta2@gmail.com>
2024-08-20 14:17:36 +0900

    #30 2 > 24.08.20 > [일어난 상황에 대한 추론] 비둘기와 전기줄

---
+++ src/tree/Lv_2/케이스별로 나누기/일어난 상황에 대한 추론.js
@@ -0,0 +1,24 @@
+// ----------------------------------------------------------------------
+/**
+ * 🔍 일어난 상황에 대한 추론 | O | 24.08.20
+ */
+const inputs = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+const N = Number(inputs[0]);
+
+let answer = 0;
+let birds = Array.from({ length: 11 }, () => -1);
+
+for (let i = 1; i <= N; i++) {
+  let [num, pos] = inputs[i].split(" ").map(Number);
+
+  if (birds[num] === -1) birds[num] = pos;
+  if (birds[num] !== -1 && birds[num] !== pos) {
+    birds[num] = pos;
+    answer += 1;
+  }
+}
+console.log(answer);
