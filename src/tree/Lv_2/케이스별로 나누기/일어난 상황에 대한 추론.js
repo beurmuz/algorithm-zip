4f995ee4be040417b5bfc2ffc218ee5559a7f3d9
beurmuz <fallinta2@gmail.com>
2024-08-22 15:40:45 +0900

    #30 2 > 24.08.22 > [일어난 상황에 대한 추론] 달리자

--- src/tree/Lv_2/케이스별로 나누기/일어난 상황에 대한 추론.js
+++ src/tree/Lv_2/케이스별로 나누기/일어난 상황에 대한 추론.js
@@ -22,3 +22,30 @@ for (let i = 1; i <= N; i++) {
   }
 }
 console.log(answer);
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 ⭐️달리자⭐️ | X | 24.08.21
+ */
+const inputs = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+const N = Number(inputs[0]);
+const before = inputs[1].split(" ").map(Number);
+const after = inputs[2].split(" ").map(Number);
+
+let answer = 0;
+
+// 사람들의 이동 거리를 최소화 시키려면, 이동할 사람의 수만큼 현재 집에서 다음 집으로 이동시키며 이동한 사람의 수를 세면 된다.
+for (let i = 0; i < N; i++) {
+  if (before[i] > after[i]) {
+    // 이동이 일어난 것
+    const diff = before[i] - after[i];
+    before[i] -= diff; // 차이만큼 빼준 후
+    before[i + 1] += diff; // 다음 집으로 옮겨준다.
+    answer += diff;
+  }
+}
+console.log(answer);
