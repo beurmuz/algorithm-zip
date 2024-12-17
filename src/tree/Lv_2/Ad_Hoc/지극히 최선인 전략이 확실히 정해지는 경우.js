559cc6650fc2c1a4a6e086894cc6cc4e014a0e6f
beurmuz <fallinta2@gmail.com>
2024-12-17 11:21:31 +0900

    #30 2 > 24.12.17 > Ad_Hoc > 최소 와이파이 수

--- src/tree/Lv_2/Ad_Hoc/지극히 최선인 전략이 확실히 정해지는 경우.js
+++ src/tree/Lv_2/Ad_Hoc/지극히 최선인 전략이 확실히 정해지는 경우.js
@@ -16,3 +16,30 @@ const answer = blocks.reduce(
   0
 );
 console.log(answer);
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 최소 와이파이 수 | O | 24.12.17
+ */
+const inputs = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+const [N, M] = inputs[0].split(" ").map(Number);
+const people = inputs[1].split(" ").map(Number);
+
+let answer = 0;
+let i = 0;
+while (i < N) {
+  if (people[i] === 1) {
+    answer += 1;
+    // console.log('wifi: ', i + M); // 와이파이 설치 장소
+    i = i + M * 2 + 1; // 와이파이는 자기 자신을 기준으로 -M ~ +M 까지 사용 가능
+  } else {
+    i += 1;
+  }
+  if (i >= N) break;
+}
+
+console.log(answer);
