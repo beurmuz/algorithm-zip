775cd13f2678be2cd050fb6600ca968f4a850b0f
beurmuz <fallinta2@gmail.com>
2024-12-30 21:59:32 +0900

    #30 3 > 24.12.30 > bound2

--- src/tree/Lv_3/이진탐색/이진탐색.js
+++ src/tree/Lv_3/이진탐색/이진탐색.js
@@ -17,3 +17,23 @@ function lower_bound(arr, target) {
   }
   return min_idx;
 }
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 bound 2 | O | 24.12.30 🔍
+ */
+function upper_bound(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  let min_idx = arr.length; // ✅ 최댓값으로 변수를 초기화한다.
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] > target) {
+      right = mid - 1; // ✅ 왼쪽 조건을 만족하는 숫자가 더 있을 수도 있으니, right를 바꿔준다.
+      min_idx = Math.min(min_idx, mid);
+    } else left = mid + 1; // 같거나 작은 경우
+  }
+  return min_idx;
+}
