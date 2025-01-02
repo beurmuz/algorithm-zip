4df9a2c060ebd296b7225eb2c2d8c003f9f65147
beurmuz <fallinta2@gmail.com>
2025-01-02 16:26:16 +0900

    #30 4 > 25.01.02 > 사각형 채우기

--- src/tree/Lv_4/DP 1/subproblem을 그대로 합치면 되는 DP.js
+++ src/tree/Lv_4/DP 1/subproblem을 그대로 합치면 되는 DP.js
@@ -42,3 +42,46 @@ function fibo(n) {
   return fibo(n - 1) + fibo(n - 2);
 }
 console.log(fibo(N));
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 계단 오르기 | △ | 25.01.02 🔍
+ */
+const N = Number(
+  require("fs").readFileSync("/dev/stdin").toString().trim().split("\n")
+);
+const dp = Array.from({ length: N + 1 }, () => 0);
+
+// 모듈러 연산을 사용해
+// 1) 배열의 값이 커지는 것을 방지
+// 2) 결과값을 특정 범위 (0~10006) 내로 유지
+// 3) 연산 속도를 최적화하고, 메모리 사용을 줄이기 위함
+const MOD = 10007;
+
+dp[2] = 1;
+dp[3] = 1;
+for (let i = 4; i <= N; i++) {
+  dp[i] = (dp[i - 2] + dp[i - 3]) % MOD; // 2계단 or 3계단을 점프해서 올라온 가지수
+}
+
+console.log(dp[N]);
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 사각형 채우기 |  | 25.01.02 🔍
+ */
+const N = Number(
+  require("fs").readFileSync("/dev/stdin").toString().trim().split("\n")
+);
+const dp = Array.from({ length: N + 1 }, () => 0);
+
+const MOD = 10007;
+
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= N; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+}
+
+console.log(dp[N]);
