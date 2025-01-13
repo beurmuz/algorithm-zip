2c88232d2f2a401b55da225001ec26e99fa85fdd
beurmuz <fallinta2@gmail.com>
2025-01-13 20:43:25 +0900

    #30 4 > 25.01.13 > 정수 사각형 최소 합

--- src/tree/Lv_4/DP 1/격자 안에서 한 칸씩 전진하는 DP.js
+++ src/tree/Lv_4/DP 1/격자 안에서 한 칸씩 전진하는 DP.js
@@ -43,6 +43,7 @@ for (let i = 1; i <= N; i++) {
   arr.push(inputs[i].split(" ").map(Number));
 }
 
+// ✅ Tabulation
 const dp = Array.from({ length: N }, () => Array(N).fill(0));
 
 // 1. 초기값 세팅
@@ -64,3 +65,42 @@ for (let i = 1; i < N; i++) {
   }
 }
 console.log(dp[N - 1][N - 1]);
+
+// ✅ Backtracking
+
+// ✅ Memoization
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 정수 사각형 최소 합 | O | 25.01.13 🔍
+ */
+const inputs = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+const N = Number(inputs[0]);
+const arr = [];
+for (let i = 1; i <= N; i++) {
+  arr.push(inputs[i].split(" ").map(Number));
+}
+
+const dp = Array.from({ length: N }, () => Array(N).fill(0));
+dp[0][N - 1] = arr[0][N - 1];
+
+for (let i = 1; i < N; i++) {
+  dp[i][N - 1] = dp[i - 1][N - 1] + arr[i][N - 1];
+}
+
+for (let i = N - 2; i >= 0; i--) {
+  dp[0][i] = dp[0][i + 1] + arr[0][i];
+}
+
+// 점화식으로 남은 칸 채우기
+for (let i = 1; i < N; i++) {
+  // 행은 증가, 열은 감소
+  for (let j = N - 2; j >= 0; j--) {
+    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j + 1]) + arr[i][j];
+  }
+}
+console.log(dp[N - 1][0]);
