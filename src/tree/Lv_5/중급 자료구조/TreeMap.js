4c6792f242573e61ee62355a61ad5231d011a99d
beurmuz <fallinta2@gmail.com>
2025-01-20 12:25:17 +0900

    #30 5 > 25.01.20 > 비율 구하기

--- src/tree/Lv_5/중급 자료구조/TreeMap.js
+++ src/tree/Lv_5/중급 자료구조/TreeMap.js
@@ -36,3 +36,29 @@ for (let i = 1; i < inputs.length; i++) {
     }
   }
 }
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 비율 구하기 | O | 25.01.20
+ */
+const SortedMap = require("collections/sorted-map");
+
+const inputs = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+const N = Number(inputs[0]);
+
+const sortedmap = new SortedMap();
+
+for (let i = 1; i < inputs.length; i++) {
+  if (sortedmap.has(inputs[i]))
+    sortedmap.set(inputs[i], sortedmap.get(inputs[i]) + 1);
+  else sortedmap.set(inputs[i], 1);
+}
+
+for (let [key, value] of sortedmap.entries()) {
+  // 해당 문자열이 차지하는 비율을 백분율로 표현하기 (단, 소수점 4째자리까지 반올림할것)
+  console.log(key, ((value / N) * 100).toFixed(4));
+}
