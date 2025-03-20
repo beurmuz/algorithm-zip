29eaace9412314954aed27f5fcc1d1f2a59fd7bf
beurmuz <fallinta2@gmail.com>
2025-03-20 20:11:38 +0900

    #30 4 > 25.03.20 > 나이트

--- src/tree/Lv_4/BFS/가중치가 동일한 그래프에서의 BFS.js
+++ src/tree/Lv_4/BFS/가중치가 동일한 그래프에서의 BFS.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 /**
- * 🔍 최소 경로로 탈출 하기 | O | 25.03.20 🔍
- * - 맞았긴 했으나 한번 더 풀어보기!
+ * 🔍 ⭐️최소 경로로 탈출 하기⭐️ | O | 25.03.20 🔍
+ * - 정답이지만 한번 더 풀어보기!
  */
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
@@ -40,3 +40,44 @@ dist[0][0] = 0;
 bfs(0, 0);
 
 console.log(dist[n - 1][m - 1]);
+
+// ----------------------------------------------------------------------
+/**
+ * 🔍 나이트 | O | 25.03.20 🔍
+ */
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);
+let [r1, c1, r2, c2] = input[1].split(" ").map((v) => Number(v) - 1);
+
+const dist = Array.from({ length: n }, () => Array(n).fill(-1));
+const dx = [-2, -1, 1, 2, 2, 1, -1, -2];
+const dy = [1, 2, 2, 1, -1, -2, -2, -1];
+
+function inRange(x, y) {
+  return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+function bfs() {
+  while (queue.length) {
+    let [cx, cy] = queue.shift();
+
+    for (let d = 0; d < 8; d++) {
+      let nx = cx + dx[d];
+      let ny = cy + dy[d];
+
+      if (inRange(nx, ny) && dist[nx][ny] === -1) {
+        queue.push([nx, ny]);
+        dist[nx][ny] = dist[cx][cy] + 1;
+      }
+    }
+  }
+}
+
+let queue = [];
+queue.push([r1, c1]);
+dist[r1][c1] = 0;
+bfs();
+
+console.log(dist[r2][c2]);
